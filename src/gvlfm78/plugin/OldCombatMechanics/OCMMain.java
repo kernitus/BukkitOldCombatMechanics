7b7c47ae87e00cb7410cb0d595e01ff68ae718f2
gvlfm78 <gvlfm78@gmail.com>
2016-03-02 17:13:22 +0100

    Setting up

---
+++ src/gvlfm78/plugin/OldCombatMechanics/OCMMain.java
@@ -0,0 +1,44 @@
+package gvlfm78.plugin.OldCombatMechanics;
+
+import java.io.IOException;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OCMMain extends JavaPlugin{
+
+	protected OCMUpdateChecker updateChecker;
+
+	@Override
+	public void onEnable(){
+		this.updateChecker = new OCMUpdateChecker(this, "http://dev.bukkit.org/bukkit-plugins/old-combat-mechanics/files.rss");
+		this.updateChecker.updateNeeded();
+		if(getConfig().getBoolean("settings.checkForUpdates")){
+			if(this.updateChecker.updateNeeded()){
+				getLogger().info("An update of OldCombatMechanics to version " + this.updateChecker.getVersion()+"is available!");
+				getLogger().info("Click here to download it:"+this.updateChecker.getLink());
+			}
+		}
+		PluginDescriptionFile pdfFile = this.getDescription();
+		//Listeners and stuff
+		getServer().getPluginManager().registerEvents((new OCMListener(this)), this);//Firing event listener
+		getCommand("Hotels").setExecutor(new OCMCommandHandler(this));//Firing commands listener
+
+		//Logging to console the correct enabling of OCM
+		getLogger().info(pdfFile.getName()+" v"+pdfFile.getVersion()+ " has been enabled correctly");
+		//Metrics
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException e) {
+			// Failed to submit the stats :-(
+		}
+	}
+	@Override
+	public void onDisable(){
+
+		PluginDescriptionFile pdfFile = this.getDescription();
+		//Logging to console the disabling of Hotels
+		getLogger().info(pdfFile.getName() + " v" + pdfFile.getVersion() + " has been disabled");
+	}
+}
---
+++ src/gvlfm78/plugin/OldCombatMechanics/OCMUpdateChecker.java
@@ -0,0 +1,94 @@
+package gvlfm78.plugin.OldCombatMechanics;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class OCMUpdateChecker {
+
+	private OCMMain plugin;
+
+	public OCMUpdateChecker(OCMMain OUC)
+	{
+		this.plugin = OUC;
+	}
+	
+	private URL filesFeed;
+
+	private String version;
+	private String link;
+
+	public OCMUpdateChecker(OCMMain plugin, String url){
+		this.plugin = plugin;
+
+		try{
+			this.filesFeed = new URL(url);
+		}catch (MalformedURLException e){
+			e.printStackTrace();
+		}
+	}
+
+	public boolean updateNeeded(){
+		try {
+			InputStream input = this.filesFeed.openConnection().getInputStream();
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input);
+
+			Node latestFile = document.getElementsByTagName("item").item(0);
+			NodeList children = latestFile.getChildNodes();
+
+			this.version = children.item(1).getTextContent().replaceAll("[a-zA-Z ]", "");
+			this.link = children.item(3).getTextContent();
+
+			if(versionCompare(plugin.getDescription().getVersion(),this.version)<0){
+				return true;
+			}
+		} catch (UnknownHostException uhe){
+			plugin.getServer().getLogger().severe("OCM Could not establish a connection to the bukit servers");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+		return false;
+	}
+
+	public String getVersion(){
+		return this.version;
+	}
+
+	public String getLink(){
+		return this.link;
+	}
+
+	public Integer versionCompare(String oldVer, String newVer)
+	{
+		String[] vals1 = oldVer.split("\\.");
+		String[] vals2 = newVer.split("\\.");
+		int i = 0;
+		// set index to first non-equal ordinal or length of shortest version string
+		while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) 
+		{
+			i++;
+		}
+		// compare first non-equal ordinal number
+		if (i < vals1.length && i < vals2.length) 
+		{
+			int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+			return Integer.signum(diff);
+		}
+		// the strings are equal or one string is a substring of the other
+		// e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+		else
+		{
+			return Integer.signum(vals1.length - vals2.length);
+		}
+	}
+	
+}
