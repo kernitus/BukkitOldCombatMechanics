c7bdf7f0290ceca5bb57a5768acc610f367be07e
gvlfm78 <gvlfm78@gmail.com>
2016-03-05 14:10:52 +0100

    Release 0.1

--- src/gvlfm78/plugin/OldCombatMechanics/OCMCommandHandler.java
+++ src/gvlfm78/plugin/OldCombatMechanics/OCMCommandHandler.java
@@ -1,11 +1,15 @@
 package gvlfm78.plugin.OldCombatMechanics;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginDescriptionFile;
 
 public class OCMCommandHandler implements CommandExecutor{
-	
+
+	protected OCMUpdateChecker updateChecker;
+
 	private OCMMain plugin;
 	public OCMCommandHandler(OCMMain instance){
 		this.plugin = instance;
@@ -14,7 +18,19 @@ public class OCMCommandHandler implements CommandExecutor{
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(label.equalsIgnoreCase("oldcombatmechanics")||label.equalsIgnoreCase("ocm")){
-			sender.sendMessage("OldCombatMechanics version "+plugin.getDescription().getVersion());
+			PluginDescriptionFile pdf = plugin.getDescription();
+			
+			sender.sendMessage(ChatColor.GOLD+"OldCombatMechanics by gvlfm78 version "+pdf.getVersion());
+
+			//Update check
+			this.updateChecker = new OCMUpdateChecker(plugin, "http://dev.bukkit.org/bukkit-plugins/old-combat-mechanics/files.rss");
+			this.updateChecker.updateNeeded();
+			if(plugin.getConfig().getBoolean("settings.checkForUpdates")){
+				if(this.updateChecker.updateNeeded()){
+					sender.sendMessage(ChatColor.BLUE+"An update of OldCombatMechanics to version " + this.updateChecker.getVersion()+"is available!");
+					sender.sendMessage(ChatColor.BLUE+"Link to download it:"+ChatColor.GRAY+this.updateChecker.getLink());
+				}
+			}
 		}
 		return false;
 	}
