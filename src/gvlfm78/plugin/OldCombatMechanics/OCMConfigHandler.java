b7fa3902f2fe52d255921a930574cf8f7e368aab
Rayzr522 <rayzr522@users.noreply.github.com>
2016-06-09 15:59:40 -0400

    Added checking for player collisions module

--- src/gvlfm78/plugin/OldCombatMechanics/OCMConfigHandler.java
+++ src/gvlfm78/plugin/OldCombatMechanics/OCMConfigHandler.java
@@ -6,24 +6,26 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 public class OCMConfigHandler {
 	private OCMMain plugin;
-	public OCMConfigHandler(OCMMain instance){
+
+	public OCMConfigHandler(OCMMain instance) {
 		this.plugin = instance;
 	}
 
-
-	public void setupConfigyml(){
-		if(!getFile("config.yml").exists())
+	public void setupConfigyml() {
+		if (!getFile("config.yml").exists())
 			setupConfig("config.yml");
 	}
 
-	public void setupConfig(String fileName){
-		plugin.saveResource(fileName, false);		
-		plugin.getLogger().info("Config file "+fileName+" generated");
+	public void setupConfig(String fileName) {
+		plugin.saveResource(fileName, false);
+		plugin.getLogger().info("Config file " + fileName + " generated");
 	}
-	public YamlConfiguration getConfig(String fileName){
+
+	public YamlConfiguration getConfig(String fileName) {
 		return YamlConfiguration.loadConfiguration(getFile(fileName));
 	}
-	public File getFile(String fileName){
-		return new File(plugin.getDataFolder()+File.separator+fileName);
+
+	public File getFile(String fileName) {
+		return new File(plugin.getDataFolder() + File.separator + fileName);
 	}
 }
