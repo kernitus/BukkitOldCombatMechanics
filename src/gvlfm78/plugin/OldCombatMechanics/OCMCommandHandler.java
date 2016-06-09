e1562148e61031b61cdcdf36e78e459d6e856bff
kernitus <2789734+kernitus@users.noreply.github.com>
2016-06-09 20:03:34 +0200

    Fixed some bugs & added plugin toggle

--- src/gvlfm78/plugin/OldCombatMechanics/OCMCommandHandler.java
+++ src/gvlfm78/plugin/OldCombatMechanics/OCMCommandHandler.java
@@ -16,19 +16,24 @@ public class OCMCommandHandler implements CommandExecutor{
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(label.equalsIgnoreCase("oldcombatmechanics")||label.equalsIgnoreCase("ocm")){
-			OCMUpdateChecker updateChecker = new OCMUpdateChecker(plugin);
+			if(args.length>0&&args[0].equalsIgnoreCase("reload")){//Reloads config
+				plugin.reloadConfig();
+				sender.sendMessage(ChatColor.GOLD+"OldCombatMechanics "+ChatColor.GREEN+"Config file reloaded");
+			}
+			else{//Tells user about available commands
+				OCMUpdateChecker updateChecker = new OCMUpdateChecker(plugin);
 
-			PluginDescriptionFile pdf = plugin.getDescription();
+				PluginDescriptionFile pdf = plugin.getDescription();
 
-			sender.sendMessage(ChatColor.GOLD+"OldCombatMechanics by kernitus version "+pdf.getVersion());
+				sender.sendMessage(ChatColor.GOLD+"OldCombatMechanics by kernitus version "+pdf.getVersion());
+				sender.sendMessage(ChatColor.GREEN+"You can use "+ChatColor.ITALIC+"/ocm reload "+ChatColor.RESET+ChatColor.GREEN+"to reload the config file");
 
-
-			//Update check
-			if(plugin.getConfig().getBoolean("update-checker")){
-				updateChecker.updateNeeded();
-				if(updateChecker.updateNeeded()){
-					sender.sendMessage(ChatColor.BLUE+"An update of OldCombatMechanics to version " + updateChecker.getVersion()+"is available!");
-					sender.sendMessage(ChatColor.BLUE+"Link to download it:"+ChatColor.GRAY+updateChecker.getLink());
+				//Update check
+				if(plugin.getConfig().getBoolean("update-checker")){
+					if(updateChecker.updateNeeded()){
+						sender.sendMessage(ChatColor.BLUE+"An update of OldCombatMechanics to version " + updateChecker.getVersion()+" is available!");
+						sender.sendMessage(ChatColor.BLUE+"Link to download it: "+ChatColor.GRAY+updateChecker.getLink());
+					}
 				}
 			}
 		}
