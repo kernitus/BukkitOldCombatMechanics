dc0a6b4b150d82e195b88aa3b6c15dc27629425e
Rayzr522 <rayzr522@users.noreply.github.com>
2016-06-09 15:59:40 -0400

    Added checking for player collisions module

--- src/gvlfm78/plugin/OldCombatMechanics/OCMMain.java
+++ src/gvlfm78/plugin/OldCombatMechanics/OCMMain.java
@@ -2,46 +2,73 @@ package kernitus.plugin.OldCombatMechanics;
 
 import java.io.IOException;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class OCMMain extends JavaPlugin{
+import sun.security.krb5.Config;
+
+public class OCMMain extends JavaPlugin {
 
 	protected OCMUpdateChecker updateChecker = new OCMUpdateChecker(this);
 	private OCMConfigHandler CH = new OCMConfigHandler(this);
 
 	@Override
-	public void onEnable(){
+	public void onEnable() {
 		this.updateChecker.updateNeeded();
-		if(getConfig().getBoolean("settings.checkForUpdates")){
-			if(this.updateChecker.updateNeeded()){
-				getLogger().info("An update of OldCombatMechanics to version " + this.updateChecker.getVersion()+"is available!");
-				getLogger().info("Click here to download it:"+this.updateChecker.getLink());
+		if (getConfig().getBoolean("settings.checkForUpdates")) {
+			if (this.updateChecker.updateNeeded()) {
+				getLogger().info("An update of OldCombatMechanics to version " + this.updateChecker.getVersion()
+						+ "is available!");
+				getLogger().info("Click here to download it:" + this.updateChecker.getLink());
 			}
 		}
 		PluginDescriptionFile pdfFile = this.getDescription();
-		//Listeners and stuff
-		getServer().getPluginManager().registerEvents((new OCMListener(this)), this);//Firing event listener
-		getCommand("OldCombatMechanics").setExecutor(new OCMCommandHandler(this));//Firing commands listener
+		// Listeners and stuff
+		getServer().getPluginManager().registerEvents((new OCMListener(this)), this);// Firing
+																						// event
+																						// listener
+		getCommand("OldCombatMechanics").setExecutor(new OCMCommandHandler(this));// Firing
+																					// commands
+																					// listener
 
-		//Setting up config.yml
+		// Setting up config.yml
 		CH.setupConfigyml();
-		
-		//Logging to console the correct enabling of OCM
-		getLogger().info(pdfFile.getName()+" v"+pdfFile.getVersion()+ " has been enabled correctly");
-		//Metrics
+
+		// Logging to console the correct enabling of OCM
+		getLogger().info(pdfFile.getName() + " v" + pdfFile.getVersion() + " has been enabled correctly");
+		// Metrics
 		try {
 			MetricsLite metrics = new MetricsLite(this);
 			metrics.start();
 		} catch (IOException e) {
 			// Failed to submit the stats :-(
 		}
+
+		// Do some checks to see if certain features are available:
+		try {
+
+			if (Player.class.getMethod("setCollidable", Boolean.class) == null) {
+
+				getConfig().set("disable-player-collisions", false);
+				saveConfig();
+
+				getLogger().warning(
+						"Could not disable player collisions in this version. Please upgrade to Spigot/Bukkit 1.9.4 or greater.");
+
+			}
+
+		} catch (Exception e) {
+			// Not entirely sure what to say here.
+			e.printStackTrace();
+		}
 	}
+
 	@Override
-	public void onDisable(){
+	public void onDisable() {
 
 		PluginDescriptionFile pdfFile = this.getDescription();
-		//Logging to console the disabling of Hotels
+		// Logging to console the disabling of Hotels
 		getLogger().info(pdfFile.getName() + " v" + pdfFile.getVersion() + " has been disabled");
 	}
 }
