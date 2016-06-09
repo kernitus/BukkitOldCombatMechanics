2f44b1f379700734d8a8b54f577c6bbc4fbea3d9
Peter Blood <rayzr522@users.noreply.github.com>
2016-06-09 15:51:42 -0400

    Added ability to toggle player collision

--- src/gvlfm78/plugin/OldCombatMechanics/OCMListener.java
+++ src/gvlfm78/plugin/OldCombatMechanics/OCMListener.java
@@ -1,6 +1,5 @@
 package kernitus.plugin.OldCombatMechanics;
 
-
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,45 +9,57 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class OCMListener implements Listener{
+public class OCMListener implements Listener {
 
 	private OCMMain plugin;
-	public OCMListener(OCMMain instance){
+
+	public OCMListener(OCMMain instance) {
 		this.plugin = instance;
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerLogin(PlayerJoinEvent e){
+	public void onPlayerLogin(PlayerJoinEvent e) {
 		OCMUpdateChecker updateChecker = new OCMUpdateChecker(plugin);
 		Player p = e.getPlayer();
 		FileConfiguration config = plugin.getConfig();
 
-		//Checking for updates
-		if(p.isOp()){
-			if(config.getBoolean("update-checker")){
-				if(plugin.getConfig().getBoolean("settings.checkForUpdates")){
-					if(updateChecker.updateNeeded()){
-						p.sendMessage("An update for OldCombatMechanics to version " + updateChecker.getVersion()+" is available!");
-						p.sendMessage("Click here to download it: "+updateChecker.getLink());
+		// Checking for updates
+		if (p.isOp()) {
+			if (config.getBoolean("update-checker")) {
+				if (plugin.getConfig().getBoolean("settings.checkForUpdates")) {
+					if (updateChecker.updateNeeded()) {
+						p.sendMessage("An update for OldCombatMechanics to version " + updateChecker.getVersion()
+								+ " is available!");
+						p.sendMessage("Click here to download it: " + updateChecker.getLink());
 					}
 				}
 			}
 		}
-		if(config.getBoolean("plugin-active")){//Setting to no cooldown
+		if (config.getBoolean("plugin-active")) {// Setting to no cooldown
 			AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
-			double baseValue= attribute.getBaseValue();
-			if(baseValue!=1024){
+			double baseValue = attribute.getBaseValue();
+			if (baseValue != 1024) {
 				attribute.setBaseValue(1024);
 				p.saveData();
 			}
-		}
-		else{//Re-enabling cooldown
+		} else {// Re-enabling cooldown
 			AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
-			double baseValue= attribute.getBaseValue();
-			if(baseValue!=4){
+			double baseValue = attribute.getBaseValue();
+			if (baseValue != 4) {
 				attribute.setBaseValue(4);
 				p.saveData();
 			}
 		}
+		
+		if (config.getBoolean("disable-player-collisions")) {
+			
+			p.setCollidable(false);
+		
+		} else {
+			
+			p.setCollidable(true);
+			
+		}
+		
 	}
 }
