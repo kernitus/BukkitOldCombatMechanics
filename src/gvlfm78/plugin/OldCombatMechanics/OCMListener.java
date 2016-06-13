7659356519ae5ec0cdf705930da1057271320b86
Rayzr522 <rayzr522@users.noreply.github.com>
2016-06-13 15:27:28 -0400

    Added permission "OldCombatMechanics.notify"

--- src/gvlfm78/plugin/OldCombatMechanics/OCMListener.java
+++ src/gvlfm78/plugin/OldCombatMechanics/OCMListener.java
@@ -12,19 +12,23 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class OCMListener implements Listener {
 
 	private OCMMain plugin;
+	private FileConfiguration config;
 
 	public OCMListener(OCMMain instance) {
+
 		this.plugin = instance;
+		this.config = plugin.getConfig();
+
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerLogin(PlayerJoinEvent e) {
+
 		OCMUpdateChecker updateChecker = new OCMUpdateChecker(plugin);
 		Player p = e.getPlayer();
-		FileConfiguration config = plugin.getConfig();
 
 		// Checking for updates
-		if (p.isOp()) {
+		if (p.hasPermission("OldCombatMechanics.notify")) {
 			updateChecker.sendUpdateMessages(p);
 		}
 
@@ -45,4 +49,5 @@ public class OCMListener implements Listener {
 		}
 
 	}
+
 }
