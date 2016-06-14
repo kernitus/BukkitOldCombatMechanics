e0c7eeea59ff2b40279694b0704830a5b3f02602
Rayzr522 <rayzr522@users.noreply.github.com>
2016-06-14 13:53:55 -0400

    Added more framework for old-axe-damage

---
+++ src/gvlfm78/plugin/OldCombatMechanics/WeaponDamages.java
@@ -0,0 +1,22 @@
+package gvlfm78.plugin.OldCombatMechanics;
+
+/**
+ * Created by Rayzr522 on 6/14/16.
+ */
+public enum WeaponDamages {
+    GOLD_AXE(4),
+    WOOD_AXE(4),
+    STONE_AXE(5),
+    IRON_AXE(6),
+    DIAMOND_AXE(7);
+
+    private int damage;
+
+    WeaponDamages(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+}
