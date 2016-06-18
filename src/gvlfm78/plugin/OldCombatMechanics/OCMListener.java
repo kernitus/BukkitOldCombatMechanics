f960430adb70dd47c138030df517e437a0c51c29
Rayzr522 <rayzr522@users.noreply.github.com>
2016-06-18 12:24:38 -0400

    Added missing logic to onPlayerJoin

--- src/gvlfm78/plugin/OldCombatMechanics/OCMListener.java
+++ src/gvlfm78/plugin/OldCombatMechanics/OCMListener.java
@@ -14,122 +14,124 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class OCMListener implements Listener {
 
-	private OCMMain plugin;
-	private FileConfiguration config;
+    private OCMMain plugin;
+    private FileConfiguration config;
 
-	public OCMListener(OCMMain instance) {
+    public OCMListener(OCMMain instance) {
 
-		this.plugin = instance;
-		this.config = plugin.getConfig();
+        this.plugin = instance;
+        this.config = plugin.getConfig();
 
-	}
-	
-	OCMTask task = new OCMTask(plugin);
+    }
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerLogin(PlayerJoinEvent e) {
+    OCMTask task = new OCMTask(plugin);
 
-		OCMUpdateChecker updateChecker = new OCMUpdateChecker(plugin);
-		Player p = e.getPlayer();
-		World world = p.getWorld();
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerLogin(PlayerJoinEvent e) {
 
-		// Checking for updates
-		if (p.hasPermission("OldCombatMechanics.notify")) {
-			updateChecker.sendUpdateMessages(p);
-		}
+        OCMUpdateChecker updateChecker = new OCMUpdateChecker(plugin);
+        Player p = e.getPlayer();
+        World world = p.getWorld();
 
-		int GAS = plugin.getConfig().getInt("disable-attack-cooldown.general-atack-speed");
-		
-		if (Config.moduleEnabled("disable-attack-cooldown",world)) {// Setting to no cooldown
-			AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
-			double baseValue = attribute.getBaseValue();
-			if (baseValue!=GAS) {
-				attribute.setBaseValue(GAS);
-				p.saveData();
-			}
-		} else {// Re-enabling cooldown
-			AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
-			double baseValue = attribute.getBaseValue();
-			if (baseValue!=4) {
-				attribute.setBaseValue(4);
-				p.saveData();
-			}
-		}
-		if(Config.moduleEnabled("disable-player-collisions"))
-			task.addPlayerToScoreboard(p);
-	}
+        // Checking for updates
+        if (p.hasPermission("OldCombatMechanics.notify")) {
+            updateChecker.sendUpdateMessages(p);
+        }
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onWorldChange(PlayerChangedWorldEvent e){
-		Player player = e.getPlayer();
-		World world = player.getWorld();
+        int GAS = plugin.getConfig().getInt("disable-attack-cooldown.general-atack-speed");
 
-		AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
-		double baseValue = attribute.getBaseValue();
+        if (Config.moduleEnabled("disable-attack-cooldown", world)) {// Setting to no cooldown
+            AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+            double baseValue = attribute.getBaseValue();
+            if (baseValue != GAS) {
+                attribute.setBaseValue(GAS);
+                p.saveData();
+            }
+        } else {// Re-enabling cooldown
+            AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+            double baseValue = attribute.getBaseValue();
+            if (baseValue != 4) {
+                attribute.setBaseValue(4);
+                p.saveData();
+            }
+        }
+        if (Config.moduleEnabled("disable-player-collisions")) {
+            task.addPlayerToScoreboard(p);
+        } else {
+            task.removePlayerFromScoreboard(p);
+        }
+    }
 
-		if (Config.moduleEnabled("disable-attack-cooldown",world)){//Disabling cooldown
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onWorldChange(PlayerChangedWorldEvent e) {
+        Player player = e.getPlayer();
+        World world = player.getWorld();
 
-			int GAS = plugin.getConfig().getInt("disable-attack-cooldown.general-atack-speed");
-			
-			if (baseValue!=GAS){
-				attribute.setBaseValue(GAS);
-				player.saveData();
-			}
-		}
-		else{//Re-enabling cooldown
-			if (baseValue!=4){
-				attribute.setBaseValue(4);
-				player.saveData();
-			}
-		}
+        AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+        double baseValue = attribute.getBaseValue();
 
-		if(Config.moduleEnabled("disable-player-collisions", world))
-			task.addPlayerToScoreboard(player);
-		else {
-			task.removePlayerFromScoreboard(player);
-		}
-	}
-	
-	// Add when finished:
-	// @EventHandler(priority = EventPriority.HIGH)
-	public void onEntityDamaged(EntityDamageByEntityEvent e) {
-		World world = e.getDamager().getWorld();
+        if (Config.moduleEnabled("disable-attack-cooldown", world)) {//Disabling cooldown
 
-		// Add '|| !moduleEnabled("disable-knockback-attack")' when you add that feature
-		if (!Config.moduleEnabled("old-tool-damage", world)) {
-			return;
-		}
+            int GAS = plugin.getConfig().getInt("disable-attack-cooldown.general-atack-speed");
 
-		if (!(e.getDamager() instanceof Player)) {
-			return;
-		}
+            if (baseValue != GAS) {
+                attribute.setBaseValue(GAS);
+                player.saveData();
+            }
+        } else {//Re-enabling cooldown
+            if (baseValue != 4) {
+                attribute.setBaseValue(4);
+                player.saveData();
+            }
+        }
 
-		Player p = (Player) e.getDamager();
-		
-		if (isHolding(p, "axe")) {
+        if (Config.moduleEnabled("disable-player-collisions", world))
+            task.addPlayerToScoreboard(player);
+        else {
+            task.removePlayerFromScoreboard(player);
+        }
+    }
 
-			onAxeAttack(e,p);
+    // Add when finished:
+    // @EventHandler(priority = EventPriority.HIGH)
+    public void onEntityDamaged(EntityDamageByEntityEvent e) {
+        World world = e.getDamager().getWorld();
 
-		}
+        // Add '|| !moduleEnabled("disable-knockback-attack")' when you add that feature
+        if (!Config.moduleEnabled("old-tool-damage", world)) {
+            return;
+        }
 
-	}
+        if (!(e.getDamager() instanceof Player)) {
+            return;
+        }
 
-	private void onAxeAttack(EntityDamageByEntityEvent e, Player p) {
-		AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
-		double baseValue = attribute.getBaseValue();
-		//Get item in main hand, check if one of following types and set new base value accordingly
-		//Stone 4
-		//Iron 5
-		//Diamond 6
-		//Gold,wood 3
-	    attribute.setBaseValue(6);
-	}
+        Player p = (Player) e.getDamager();
 
-	private void onSwordAttack() {
-	}
+        if (isHolding(p, "axe")) {
 
-	private boolean isHolding(Player p, String type) {
-		return p.getInventory().getItemInMainHand().getType().toString().endsWith("_" + type.toUpperCase());
-	}
+            onAxeAttack(e, p);
+
+        }
+
+    }
+
+    private void onAxeAttack(EntityDamageByEntityEvent e, Player p) {
+        AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
+        double baseValue = attribute.getBaseValue();
+        //Get item in main hand, check if one of following types and set new base value accordingly
+        //Stone 4
+        //Iron 5
+        //Diamond 6
+        //Gold,wood 3
+        attribute.setBaseValue(6);
+    }
+
+    private void onSwordAttack() {
+    }
+
+    private boolean isHolding(Player p, String type) {
+        return p.getInventory().getItemInMainHand().getType().toString().endsWith("_" + type.toUpperCase());
+    }
 
 }
