73918648b2ce9847b622011f3950fcd7cb9c9ae4
Rayzr522 <rayzr522@users.noreply.github.com>
2016-06-25 08:32:16 -0400

    Added module classes to git

---
+++ src/gvlfm78/plugin/OldCombatMechanics/module/Module.java
@@ -0,0 +1,17 @@
+package gvlfm78.plugin.OldCombatMechanics.module;
+
+import gvlfm78.plugin.OldCombatMechanics.OCMMain;
+import org.bukkit.event.Listener;
+
+/**
+ * Created by Rayzr522 on 6/25/16.
+ */
+public class Module implements Listener {
+
+    protected OCMMain plugin;
+
+    public Module(OCMMain plugin) {
+        this.plugin = plugin;
+    }
+
+}
---
+++ src/gvlfm78/plugin/OldCombatMechanics/module/ModuleAttackCooldown.java
@@ -0,0 +1,85 @@
+package gvlfm78.plugin.OldCombatMechanics.module;
+
+import gvlfm78.plugin.OldCombatMechanics.Config;
+import gvlfm78.plugin.OldCombatMechanics.OCMMain;
+import gvlfm78.plugin.OldCombatMechanics.OCMUpdateChecker;
+import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * Created by Rayzr522 on 6/25/16.
+ */
+public class ModuleAttackCooldown extends Module {
+
+    public ModuleAttackCooldown(OCMMain plugin) {
+        super(plugin);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerLogin(PlayerJoinEvent e) {
+
+        OCMUpdateChecker updateChecker = new OCMUpdateChecker(plugin);
+        Player p = e.getPlayer();
+        World world = p.getWorld();
+
+        // Checking for updates
+        if (p.hasPermission("OldCombatMechanics.notify")) {
+            updateChecker.sendUpdateMessages(p);
+        }
+
+        double GAS = plugin.getConfig().getDouble("disable-attack-cooldown.general-attack-speed");
+
+        if (Config.moduleEnabled("disable-attack-cooldown", world)) {// Setting to no cooldown
+            AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+            double baseValue = attribute.getBaseValue();
+            if (baseValue != GAS) {
+                attribute.setBaseValue(GAS);
+                p.saveData();
+            }
+        } else {// Re-enabling cooldown
+            AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+            double baseValue = attribute.getBaseValue();
+            if (baseValue != 4) {
+                attribute.setBaseValue(4);
+                p.saveData();
+            }
+        }
+
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onWorldChange(PlayerChangedWorldEvent e) {
+
+        Player player = e.getPlayer();
+        World world = player.getWorld();
+
+        AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+        double baseValue = attribute.getBaseValue();
+
+        if (Config.moduleEnabled("disable-attack-cooldown", world)) {//Disabling cooldown
+
+            double GAS = plugin.getConfig().getDouble("disable-attack-cooldown.general-attack-speed");
+
+            if (baseValue != GAS) {
+                attribute.setBaseValue(GAS);
+                player.saveData();
+            }
+
+        } else {//Re-enabling cooldown
+
+            if (baseValue != 4) {
+                attribute.setBaseValue(4);
+                player.saveData();
+            }
+
+        }
+
+    }
+
+}
