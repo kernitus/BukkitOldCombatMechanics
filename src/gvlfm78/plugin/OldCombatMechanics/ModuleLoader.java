674e2a1ef8809e99feb7a63dec1f7dc7092b1f1a
Rayzr522 <rayzr522@users.noreply.github.com>
2016-06-27 08:22:34 -0400

    Added ModuleLoader class

---
+++ src/gvlfm78/plugin/OldCombatMechanics/ModuleLoader.java
@@ -0,0 +1,81 @@
+package gvlfm78.plugin.OldCombatMechanics;
+
+import gvlfm78.plugin.OldCombatMechanics.module.Module;
+import org.bukkit.event.HandlerList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Rayzr522 on 6/27/16.
+ */
+public class ModuleLoader {
+
+    private static OCMMain plugin;
+
+    private static List<Module> modules = new ArrayList<>();
+    private static HashMap<Module, Boolean> enabledModules = null;
+
+    public static void Initialize(OCMMain plugin) {
+        ModuleLoader.plugin = plugin;
+    }
+
+    public static void ToggleModules() {
+
+        if (enabledModules == null) {
+
+            enabledModules = new HashMap<>();
+
+            for (Module module : modules) {
+
+                enabledModules.put(module, module.isEnabled());
+                SetState(module, module.isEnabled());
+
+            }
+
+        } else {
+
+            for (Module module : modules) {
+
+                if (!enabledModules.containsKey(module)) {
+
+                    enabledModules.put(module, module.isEnabled());
+                    SetState(module, module.isEnabled());
+
+                }
+
+                else if (module.isEnabled() != enabledModules.get(module)) {
+
+                    enabledModules.put(module, module.isEnabled());
+                    SetState(module, module.isEnabled());
+
+                }
+
+            }
+
+        }
+
+    }
+
+    private static void SetState(Module module, boolean state) {
+
+        if (state) {
+
+            plugin.getServer().getPluginManager().registerEvents(module, plugin);
+
+        } else {
+
+            HandlerList.unregisterAll(module);
+
+        }
+
+    }
+
+    public static void AddModule(Module module) {
+
+        modules.add(module);
+
+    }
+
+}
