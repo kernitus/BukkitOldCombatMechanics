6ba72dab05a04852ed5eef2d71627515a594ba24
Rayzr522 <rayzr522@users.noreply.github.com>
2016-06-27 08:07:59 -0400

    FishingKnockback module

--- src/gvlfm78/plugin/OldCombatMechanics/module/ModuleFishingKnockback.java
+++ src/gvlfm78/plugin/OldCombatMechanics/module/ModuleFishingKnockback.java
@@ -1,6 +1,17 @@
 package kernitus.plugin.OldCombatMechanics.module;
 
 import kernitus.plugin.OldCombatMechanics.OCMMain;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.FishHook;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.ProjectileHitEvent;
+
+import java.util.Collection;
 
 /**
  * Created by Rayzr522 on 6/27/16.
@@ -9,4 +20,43 @@ public class ModuleFishingKnockback extends Module {
     public ModuleFishingKnockback(OCMMain plugin) {
         super(plugin, "old-fishing-knockback");
     }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onRodLand(ProjectileHitEvent e) {
+
+        if (!isEnabled(e.getEntity().getWorld())) {
+            return;
+        }
+
+        if (!(e.getEntityType() == EntityType.FISHING_HOOK)) {
+            return;
+        }
+
+        Collection<Entity> entities = Bukkit.getWorld(e.getEntity().getLocation().getWorld().getName()).getNearbyEntities(e.getEntity().getLocation(), 0.25, 0.25, 0.25);
+
+        for (Entity entity : entities) {
+
+            if (!(entity instanceof Player)) {
+                continue;
+            }
+
+            FishHook hook = (FishHook) e.getEntity();
+            Player rodder = (Player) hook.getShooter();
+            Player player = (Player) entity;
+
+            if (player.getUniqueId() == rodder.getUniqueId()) {
+                continue;
+            }
+
+            player.damage(0.2);
+            Location loc = player.getLocation().add(0, 0.5, 0);
+            player.teleport(loc);
+            player.setVelocity(loc.subtract(rodder.getLocation()).toVector().normalize().multiply(0.4));
+
+            return;
+
+        }
+
+    }
+
 }
