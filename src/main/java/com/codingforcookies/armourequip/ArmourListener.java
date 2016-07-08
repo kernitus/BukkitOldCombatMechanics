a95f8e7ce1086f448b32579d63713dd85c367047
kernitus <2789734+kernitus@users.noreply.github.com>
2016-07-08 16:03:43 +0200

    Created OldArmourStrength Module class

--- src/main/java/com/codingforcookies/armourequip/ArmourListener.java
+++ src/main/java/com/codingforcookies/armourequip/ArmourListener.java
@@ -1,6 +1,7 @@
-package com.codingforcookies.armorequip;
+package com.codingforcookies.armourequip;
 
-import com.codingforcookies.armorequip.ArmorEquipEvent.EquipMethod;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -20,7 +21,10 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.List;
+import com.codingforcookies.armourequip.ArmourEquipEvent.EquipMethod;
+
+import kernitus.plugin.OldCombatMechanics.OCMMain;
+import kernitus.plugin.OldCombatMechanics.module.Module;
 
 /**
  * @Author Borlea
@@ -28,12 +32,10 @@ import java.util.List;
  * @Website http://codingforcookies.com/
  * @since Jul 30, 2015 6:43:34 PM
  */
-public class ArmorListener implements Listener{
+public class ArmourListener extends Module implements Listener{
 
-	private final List<String> blockedMaterials;
-
-	public ArmorListener(List<String> blockedMaterials){
-		this.blockedMaterials = blockedMaterials;
+	public ArmourListener(OCMMain plugin){
+		super(plugin, "old-armour-strength");
 	}
 
 	@EventHandler
@@ -49,55 +51,55 @@ public class ArmorListener implements Listener{
 		if((e.getSlotType() != SlotType.ARMOR || e.getSlotType() != SlotType.QUICKBAR) && !e.getInventory().getType().equals(InventoryType.CRAFTING)) return;
 		if(!(e.getWhoClicked() instanceof Player)) return;
 		if(e.getCurrentItem() == null) return;
-		ArmorType newArmorType = ArmorType.matchType(shift ? e.getCurrentItem() : e.getCursor());
-		if(!shift && newArmorType != null && e.getRawSlot() != newArmorType.getSlot()){
+		ArmourType newArmourType = ArmourType.matchType(shift ? e.getCurrentItem() : e.getCursor());
+		if(!shift && newArmourType != null && e.getRawSlot() != newArmourType.getSlot()){
 			// Used for drag and drop checking to make sure you aren't trying to place a helmet in the boots place.
 			return;
 		}
 		if(shift){
-			newArmorType = ArmorType.matchType(e.getCurrentItem());
-			if(newArmorType != null){
+			newArmourType = ArmourType.matchType(e.getCurrentItem());
+			if(newArmourType != null){
 				boolean equipping = true;
-				if(e.getRawSlot() == newArmorType.getSlot()){
+				if(e.getRawSlot() == newArmourType.getSlot()){
 					equipping = false;
 				}
-				if(newArmorType.equals(ArmorType.HELMET) && (equipping ? e.getWhoClicked().getInventory().getHelmet() == null : e.getWhoClicked().getInventory().getHelmet() != null) || newArmorType.equals(ArmorType.CHESTPLATE) && (equipping ? e.getWhoClicked().getInventory().getChestplate() == null : e.getWhoClicked().getInventory().getChestplate() != null) || newArmorType.equals(ArmorType.LEGGINGS) && (equipping ? e.getWhoClicked().getInventory().getLeggings() == null : e.getWhoClicked().getInventory().getLeggings() != null) || newArmorType.equals(ArmorType.BOOTS) && (equipping ? e.getWhoClicked().getInventory().getBoots() == null : e.getWhoClicked().getInventory().getBoots() != null)){
-					ArmorEquipEvent armorEquipEvent = new ArmorEquipEvent((Player) e.getWhoClicked(), EquipMethod.SHIFT_CLICK, newArmorType, equipping ? null : e.getCurrentItem(), equipping ? e.getCurrentItem() : null);
-					Bukkit.getServer().getPluginManager().callEvent(armorEquipEvent);
-					if(armorEquipEvent.isCancelled()){
+				if(newArmourType.equals(ArmourType.HELMET) && (equipping ? e.getWhoClicked().getInventory().getHelmet() == null : e.getWhoClicked().getInventory().getHelmet() != null) || newArmourType.equals(ArmourType.CHESTPLATE) && (equipping ? e.getWhoClicked().getInventory().getChestplate() == null : e.getWhoClicked().getInventory().getChestplate() != null) || newArmourType.equals(ArmourType.LEGGINGS) && (equipping ? e.getWhoClicked().getInventory().getLeggings() == null : e.getWhoClicked().getInventory().getLeggings() != null) || newArmourType.equals(ArmourType.BOOTS) && (equipping ? e.getWhoClicked().getInventory().getBoots() == null : e.getWhoClicked().getInventory().getBoots() != null)){
+					ArmourEquipEvent armourEquipEvent = new ArmourEquipEvent((Player) e.getWhoClicked(), EquipMethod.SHIFT_CLICK, newArmourType, equipping ? null : e.getCurrentItem(), equipping ? e.getCurrentItem() : null);
+					Bukkit.getServer().getPluginManager().callEvent(armourEquipEvent);
+					if(armourEquipEvent.isCancelled()){
 						e.setCancelled(true);
 					}
 				}
 			}
 		}else{
-			ItemStack newArmorPiece = e.getCursor();
-			ItemStack oldArmorPiece = e.getCurrentItem();
+			ItemStack newArmourPiece = e.getCursor();
+			ItemStack oldArmourPiece = e.getCurrentItem();
 			if(numberkey){
 				if(e.getInventory().getType().equals(InventoryType.PLAYER)){// Prevents s**t in the 2by2 crafting
 					// e.getInventory() == The players inventory
 					// e.getHotBarButton() == key people are pressing to equip or unequip the item to or from.
 					// e.getRawSlot() == The slot the item is going to.
-					// e.getSlot() == Armor slot, can't use e.getRawSlot() as that gives a hotbar slot ;-;
+					// e.getSlot() == Armour slot, can't use e.getRawSlot() as that gives a hotbar slot ;-;
 					ItemStack hotbarItem = e.getInventory().getItem(e.getHotbarButton());
 					if(hotbarItem != null){// Equipping
-						newArmorType = ArmorType.matchType(hotbarItem);
-						newArmorPiece = hotbarItem;
-						oldArmorPiece = e.getInventory().getItem(e.getSlot());
+						newArmourType = ArmourType.matchType(hotbarItem);
+						newArmourPiece = hotbarItem;
+						oldArmourPiece = e.getInventory().getItem(e.getSlot());
 					}else{// Unequipping
-						newArmorType = ArmorType.matchType(e.getCurrentItem() != null && e.getCurrentItem().getType() != Material.AIR ? e.getCurrentItem() : e.getCursor());
+						newArmourType = ArmourType.matchType(e.getCurrentItem() != null && e.getCurrentItem().getType() != Material.AIR ? e.getCurrentItem() : e.getCursor());
 					}
 				}
 			}else{
 				// e.getCurrentItem() == Unequip
 				// e.getCursor() == Equip
-				newArmorType = ArmorType.matchType(e.getCurrentItem() != null && e.getCurrentItem().getType() != Material.AIR ? e.getCurrentItem() : e.getCursor());
+				newArmourType = ArmourType.matchType(e.getCurrentItem() != null && e.getCurrentItem().getType() != Material.AIR ? e.getCurrentItem() : e.getCursor());
 			}
-			if(newArmorType != null && e.getRawSlot() == newArmorType.getSlot()){
+			if(newArmourType != null && e.getRawSlot() == newArmourType.getSlot()){
 				EquipMethod method = EquipMethod.DRAG;
 				if(e.getAction().equals(InventoryAction.HOTBAR_SWAP) || numberkey) method = EquipMethod.HOTBAR_SWAP;
-				ArmorEquipEvent armorEquipEvent = new ArmorEquipEvent((Player) e.getWhoClicked(), method, newArmorType, oldArmorPiece, newArmorPiece);
-				Bukkit.getServer().getPluginManager().callEvent(armorEquipEvent);
-				if(armorEquipEvent.isCancelled()){
+				ArmourEquipEvent armourEquipEvent = new ArmourEquipEvent((Player) e.getWhoClicked(), method, newArmourType, oldArmourPiece, newArmourPiece);
+				Bukkit.getServer().getPluginManager().callEvent(armourEquipEvent);
+				if(armourEquipEvent.isCancelled()){
 					e.setCancelled(true);
 				}
 			}
@@ -110,18 +112,15 @@ public class ArmorListener implements Listener{
 		if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK){
 			final Player player = e.getPlayer();
 			if(e.getClickedBlock() != null && e.getAction() == Action.RIGHT_CLICK_BLOCK){// Having both of these checks is useless, might as well do it though.
-				// Some blocks have actions when you right click them which stops the client from equipping the armor in hand.
+				// Some blocks have actions when you right click them which stops the client from equipping the armour in hand.
 				Material mat = e.getClickedBlock().getType();
-				for(String s : blockedMaterials){
-					if(mat.name().equalsIgnoreCase(s)) return;
-				}
 			}
-			ArmorType newArmorType = ArmorType.matchType(e.getItem());
-			if(newArmorType != null){
-				if(newArmorType.equals(ArmorType.HELMET) && e.getPlayer().getInventory().getHelmet() == null || newArmorType.equals(ArmorType.CHESTPLATE) && e.getPlayer().getInventory().getChestplate() == null || newArmorType.equals(ArmorType.LEGGINGS) && e.getPlayer().getInventory().getLeggings() == null || newArmorType.equals(ArmorType.BOOTS) && e.getPlayer().getInventory().getBoots() == null){
-					ArmorEquipEvent armorEquipEvent = new ArmorEquipEvent(e.getPlayer(), EquipMethod.HOTBAR, ArmorType.matchType(e.getItem()), null, e.getItem());
-					Bukkit.getServer().getPluginManager().callEvent(armorEquipEvent);
-					if(armorEquipEvent.isCancelled()){
+			ArmourType newArmourType = ArmourType.matchType(e.getItem());
+			if(newArmourType != null){
+				if(newArmourType.equals(ArmourType.HELMET) && e.getPlayer().getInventory().getHelmet() == null || newArmourType.equals(ArmourType.CHESTPLATE) && e.getPlayer().getInventory().getChestplate() == null || newArmourType.equals(ArmourType.LEGGINGS) && e.getPlayer().getInventory().getLeggings() == null || newArmourType.equals(ArmourType.BOOTS) && e.getPlayer().getInventory().getBoots() == null){
+					ArmourEquipEvent armourEquipEvent = new ArmourEquipEvent(e.getPlayer(), EquipMethod.HOTBAR, ArmourType.matchType(e.getItem()), null, e.getItem());
+					Bukkit.getServer().getPluginManager().callEvent(armourEquipEvent);
+					if(armourEquipEvent.isCancelled()){
 						e.setCancelled(true);
 						player.updateInventory();
 					}
@@ -132,20 +131,20 @@ public class ArmorListener implements Listener{
 
 	@EventHandler
 	public void dispenserFireEvent(BlockDispenseEvent e){
-		ArmorType type = ArmorType.matchType(e.getItem());
-		if(ArmorType.matchType(e.getItem()) != null){
+		ArmourType type = ArmourType.matchType(e.getItem());
+		if(ArmourType.matchType(e.getItem()) != null){
 			Location loc = e.getBlock().getLocation();
 			for(Player p : loc.getWorld().getPlayers()){
 				if(loc.getBlockY() - p.getLocation().getBlockY() >= -1 && loc.getBlockY() - p.getLocation().getBlockY() <= 1){
-					if(p.getInventory().getHelmet() == null && type.equals(ArmorType.HELMET) || p.getInventory().getChestplate() == null && type.equals(ArmorType.CHESTPLATE) || p.getInventory().getLeggings() == null && type.equals(ArmorType.LEGGINGS) || p.getInventory().getBoots() == null && type.equals(ArmorType.BOOTS)){
+					if(p.getInventory().getHelmet() == null && type.equals(ArmourType.HELMET) || p.getInventory().getChestplate() == null && type.equals(ArmourType.CHESTPLATE) || p.getInventory().getLeggings() == null && type.equals(ArmourType.LEGGINGS) || p.getInventory().getBoots() == null && type.equals(ArmourType.BOOTS)){
 						org.bukkit.block.Dispenser dispenser = (org.bukkit.block.Dispenser) e.getBlock().getState();
 						org.bukkit.material.Dispenser dis = (org.bukkit.material.Dispenser) dispenser.getData();
 						BlockFace directionFacing = dis.getFacing();
 						// Someone told me not to do big if checks because it's hard to read, look at me doing it -_-
 						if(directionFacing == BlockFace.EAST && p.getLocation().getBlockX() != loc.getBlockX() && p.getLocation().getX() <= loc.getX() + 2.3 && p.getLocation().getX() >= loc.getX() || directionFacing == BlockFace.WEST && p.getLocation().getX() >= loc.getX() - 1.3 && p.getLocation().getX() <= loc.getX() || directionFacing == BlockFace.SOUTH && p.getLocation().getBlockZ() != loc.getBlockZ() && p.getLocation().getZ() <= loc.getZ() + 2.3 && p.getLocation().getZ() >= loc.getZ() || directionFacing == BlockFace.NORTH && p.getLocation().getZ() >= loc.getZ() - 1.3 && p.getLocation().getZ() <= loc.getZ()){
-							ArmorEquipEvent armorEquipEvent = new ArmorEquipEvent(p, EquipMethod.DISPENSER, ArmorType.matchType(e.getItem()), null, e.getItem());
-							Bukkit.getServer().getPluginManager().callEvent(armorEquipEvent);
-							if(armorEquipEvent.isCancelled()){
+							ArmourEquipEvent armourEquipEvent = new ArmourEquipEvent(p, EquipMethod.DISPENSER, ArmourType.matchType(e.getItem()), null, e.getItem());
+							Bukkit.getServer().getPluginManager().callEvent(armourEquipEvent);
+							if(armourEquipEvent.isCancelled()){
 								e.setCancelled(true);
 							}
 							return;
@@ -158,22 +157,22 @@ public class ArmorListener implements Listener{
 
 	@EventHandler
 	public void itemBreakEvent(PlayerItemBreakEvent e){
-		ArmorType type = ArmorType.matchType(e.getBrokenItem());
+		ArmourType type = ArmourType.matchType(e.getBrokenItem());
 		if(type != null){
 			Player p = e.getPlayer();
-			ArmorEquipEvent armorEquipEvent = new ArmorEquipEvent(p, EquipMethod.BROKE, type, e.getBrokenItem(), null);
-			Bukkit.getServer().getPluginManager().callEvent(armorEquipEvent);
-			if(armorEquipEvent.isCancelled()){
+			ArmourEquipEvent armourEquipEvent = new ArmourEquipEvent(p, EquipMethod.BROKE, type, e.getBrokenItem(), null);
+			Bukkit.getServer().getPluginManager().callEvent(armourEquipEvent);
+			if(armourEquipEvent.isCancelled()){
 				ItemStack i = e.getBrokenItem().clone();
 				i.setAmount(1);
 				i.setDurability((short) (i.getDurability() - 1));
-				if(type.equals(ArmorType.HELMET)){
+				if(type.equals(ArmourType.HELMET)){
 					p.getInventory().setHelmet(i);
-				}else if(type.equals(ArmorType.CHESTPLATE)){
+				}else if(type.equals(ArmourType.CHESTPLATE)){
 					p.getInventory().setChestplate(i);
-				}else if(type.equals(ArmorType.LEGGINGS)){
+				}else if(type.equals(ArmourType.LEGGINGS)){
 					p.getInventory().setLeggings(i);
-				}else if(type.equals(ArmorType.BOOTS)){
+				}else if(type.equals(ArmourType.BOOTS)){
 					p.getInventory().setBoots(i);
 				}
 			}
@@ -185,7 +184,7 @@ public class ArmorListener implements Listener{
 		Player p = e.getEntity();
 		for(ItemStack i : p.getInventory().getArmorContents()){
 			if(i != null && !i.getType().equals(Material.AIR)){
-				Bukkit.getServer().getPluginManager().callEvent(new ArmorEquipEvent(p, EquipMethod.DEATH, ArmorType.matchType(i), i, null));
+				Bukkit.getServer().getPluginManager().callEvent(new ArmourEquipEvent(p, EquipMethod.DEATH, ArmourType.matchType(i), i, null));
 				// No way to cancel a death event.
 			}
 		}
--- src/main/java/gvlfm78/plugin/OldCombatMechanics/OCMMain.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/OCMMain.java
@@ -1,7 +1,8 @@
 package kernitus.plugin.OldCombatMechanics;
 
-import kernitus.plugin.OldCombatMechanics.module.*;
-import kernitus.plugin.OldCombatMechanics.utilities.Config;
+import java.io.IOException;
+import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -12,8 +13,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
-import java.io.IOException;
-import java.util.logging.Logger;
+import com.codingforcookies.armourequip.ArmourListener;
+
+import kernitus.plugin.OldCombatMechanics.module.ModuleAttackCooldown;
+import kernitus.plugin.OldCombatMechanics.module.ModuleFishingKnockback;
+import kernitus.plugin.OldCombatMechanics.module.ModuleGoldenApple;
+import kernitus.plugin.OldCombatMechanics.module.ModuleOldArmourStrength;
+import kernitus.plugin.OldCombatMechanics.module.ModuleOldToolDamage;
+import kernitus.plugin.OldCombatMechanics.module.ModulePlayerCollisions;
+import kernitus.plugin.OldCombatMechanics.module.ModulePlayerRegen;
+import kernitus.plugin.OldCombatMechanics.module.ModuleSwordBlocking;
+import kernitus.plugin.OldCombatMechanics.module.ModuleSwordSweep;
+import kernitus.plugin.OldCombatMechanics.utilities.Config;
 
 public class OCMMain extends JavaPlugin {
 
@@ -100,6 +111,8 @@ public class OCMMain extends JavaPlugin {
 		ModuleLoader.AddModule(new ModuleFishingKnockback(this));
 		ModuleLoader.AddModule(new ModulePlayerRegen(this));
 		ModuleLoader.AddModule(new ModuleSwordBlocking(this));
+		ModuleLoader.AddModule(new ModuleOldArmourStrength(this));
+		ModuleLoader.AddModule(new ArmourListener(this));
 
 		getCommand("OldCombatMechanics").setExecutor(new OCMCommandHandler(this));// Firing commands listener
 
