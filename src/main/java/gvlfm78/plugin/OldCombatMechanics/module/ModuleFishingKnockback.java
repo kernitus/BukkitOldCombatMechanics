00fd1f46099882be340bdce02221d294e1d63e2d
kernitus <2789734+kernitus@users.noreply.github.com>
2016-09-24 18:48:33 +0200

    Fixed health not regenerating past 10 hearts

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleFishingKnockback.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleFishingKnockback.java
@@ -1,8 +1,8 @@
 package kernitus.plugin.OldCombatMechanics.module;
 
-import com.google.common.base.Functions;
-import com.google.common.collect.ImmutableMap;
-import kernitus.plugin.OldCombatMechanics.OCMMain;
+import java.util.Collection;
+import java.util.EnumMap;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
@@ -15,61 +15,62 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 
-import java.util.Collection;
-import java.util.EnumMap;
+import com.google.common.base.Functions;
+import com.google.common.collect.ImmutableMap;
+
+import kernitus.plugin.OldCombatMechanics.OCMMain;
 
 /**
  * Created by Rayzr522 on 6/27/16.
  */
 public class ModuleFishingKnockback extends Module {
-    public ModuleFishingKnockback(OCMMain plugin) {
-        super(plugin, "old-fishing-knockback");
-    }
+	public ModuleFishingKnockback(OCMMain plugin) {
+		super(plugin, "old-fishing-knockback");
+	}
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onRodLand(ProjectileHitEvent e) {
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onRodLand(ProjectileHitEvent e) {
 
-        if (!isEnabled(e.getEntity().getWorld())) {
-            return;
-        }
+		if (!isEnabled(e.getEntity().getWorld())) {
+			return;
+		}
 
-        if (!(e.getEntityType() == EntityType.FISHING_HOOK)) {
-            return;
-        }
+		if (!(e.getEntityType() == EntityType.FISHING_HOOK)) {
+			return;
+		}
 
-        Collection<Entity> entities = Bukkit.getWorld(e.getEntity().getLocation().getWorld().getName()).getNearbyEntities(e.getEntity().getLocation(), 0.25, 0.25, 0.25);
+		Collection<Entity> entities = Bukkit.getWorld(e.getEntity().getLocation().getWorld().getName()).getNearbyEntities(e.getEntity().getLocation(), 0.25, 0.25, 0.25);
 
-        for (Entity entity : entities) {
+		for (Entity entity : entities) {
 
-            if (!(entity instanceof Player)) {
-                continue;
-            }
+			if (!(entity instanceof Player)) {
+				continue;
+			}
 
-            FishHook hook = (FishHook) e.getEntity();
-            Player rodder = (Player) hook.getShooter();
-            Player player = (Player) entity;
+			FishHook hook = (FishHook) e.getEntity();
+			Player rodder = (Player) hook.getShooter();
+			Player player = (Player) entity;
 
-            if (player.getUniqueId() == rodder.getUniqueId()) {
-                continue;
-            }
+			if (player.getUniqueId() == rodder.getUniqueId()) {
+				continue;
+			}
 
-//            player.damage(0.2);
-            Bukkit.getPluginManager().callEvent(makeEvent(rodder, player));
-            Location loc = player.getLocation().add(0, 0.5, 0);
-            player.teleport(loc);
-            player.setVelocity(loc.subtract(rodder.getLocation()).toVector().normalize().multiply(0.4));
+			//player.damage(0.2);
+			Bukkit.getPluginManager().callEvent(makeEvent(rodder, player));
+			Location loc = player.getLocation().add(0, 0.5, 0);
+			player.teleport(loc);
+			player.setVelocity(loc.subtract(rodder.getLocation()).toVector().normalize().multiply(0.4));
 
-            return;
+			return;
 
-        }
+		}
 
-    }
+	}
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
 	private EntityDamageByEntityEvent makeEvent(Player rodder, Player player) {
 
-        return new EntityDamageByEntityEvent(rodder, player, EntityDamageEvent.DamageCause.ENTITY_ATTACK, new EnumMap(ImmutableMap.of(EntityDamageEvent.DamageModifier.BASE, Double.valueOf(0.2))), new EnumMap(ImmutableMap.of(EntityDamageEvent.DamageModifier.BASE, Functions.constant(Double.valueOf(0.2)))));
+		return new EntityDamageByEntityEvent(rodder, player, EntityDamageEvent.DamageCause.ENTITY_ATTACK, new EnumMap(ImmutableMap.of(EntityDamageEvent.DamageModifier.BASE, Double.valueOf(0.2))), new EnumMap(ImmutableMap.of(EntityDamageEvent.DamageModifier.BASE, Functions.constant(Double.valueOf(0.2)))));
 
-    }
+	}
 
 }
