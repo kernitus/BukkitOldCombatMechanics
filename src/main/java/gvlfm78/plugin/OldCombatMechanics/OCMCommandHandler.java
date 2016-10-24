45f434dd220d08a1366796f184ed213c0050f12d
gvlfm78 <gvlfm78@gmail.com>
2016-10-24 20:23:05 +0200

    Implemented toggle command

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/OCMCommandHandler.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/OCMCommandHandler.java
@@ -69,31 +69,22 @@ public class OCMCommandHandler implements CommandExecutor {
 					&& sender.hasPermission("oldcombatmechanics.toggle") && sender instanceof Player){
 				//Toggle their cooldown
 				Player p = (Player) sender;
-				World world = p.getWorld();
 				double GAS = plugin.getConfig().getDouble("disable-attack-cooldown.general-attack-speed");
-				
-				boolean enabled = false;
 
-				if (Config.moduleEnabled("disable-attack-cooldown", world)) {// Setting to no cooldown
-					AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
-					double baseValue = attribute.getBaseValue();
-					if (baseValue != GAS) {
-						attribute.setBaseValue(GAS);
-						p.saveData();
-						enabled = true;
-					}
-				} else {// Re-enabling cooldown
-					AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
-					double baseValue = attribute.getBaseValue();
-					if (baseValue != 4) {
-						attribute.setBaseValue(4);
-						p.saveData();
-					}
+				AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+				double baseValue = attribute.getBaseValue();
+				String message = "";
+
+				if (baseValue == GAS) {// They're enabled, let's disable it
+					GAS = 4;
+					message = "&1[OCM] &4Attack cooldown disabled";
 				}
-				if(enabled)
-					Chatter.send(p, "&aAttack cooldown enabled");
 				else
-					Chatter.send(p, "&4Attack cooldown disabled");
+					message = "&1[OCM] &aAttack cooldown enabled";
+
+				attribute.setBaseValue(GAS);
+				p.saveData();
+				Chatter.send(p, message);
 
 			} else { //Tell them about available commands
 				OCMUpdateChecker updateChecker = new OCMUpdateChecker(plugin);
@@ -104,6 +95,9 @@ public class OCMCommandHandler implements CommandExecutor {
 
 				Chatter.send(sender, "&6&lOldCombatMechanics&e by &cgvlfm78&e and &cRayzr522&e version &6" + pdf.getVersion());
 				Chatter.send(sender, "&eYou can use &c/ocm reload&e to reload the config file");
+				
+				if(plugin.getConfig().getBoolean("enableIndividualToggle") && sender.hasPermission("oldcombatmechanics.toggle") && sender instanceof Player)
+					Chatter.send(sender, "&eYou can use &c/ocm toggle&e to turn your attack cooldown on/off");
 
 				Chatter.send(sender, ChatColor.DARK_GRAY + Chatter.HORIZONTAL_BAR);
 
