dfad97f524f725e71a6339936c37a4d7f6b4b1dd
gvlfm78 <gvlfm78@gmail.com>
2017-01-08 16:43:24 +0100

    Failed making backup config copy values into new one and keep comments

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/OCMConfigHandler.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/OCMConfigHandler.java
@@ -1,8 +1,8 @@
 package gvlfm78.plugin.OldCombatMechanics;
 
-import org.bukkit.configuration.file.YamlConfiguration;
-
 import java.io.File;
+
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class OCMConfigHandler {
 	private OCMMain plugin;
@@ -13,9 +13,86 @@ public class OCMConfigHandler {
 
 	public void upgradeConfig(){
 		if(doesConfigymlExist()){
+			//First we change name to the old config
 			File backup = getFile("config-backup.yml");
 			if(backup.exists()) backup.delete();
-			getFile("config.yml").renameTo(backup);
+			File configFile = getFile("config.yml");
+			configFile.renameTo(backup);
+			//Then we save the new version
+			plugin.saveResource("config.yml", false);
+			
+			//The following bit doesn't work and is awfully complicated so it's commented out for the future
+			
+			//Then we loop through the old version
+			//Check if the value set for each node exists in the new one,
+			//and if different, set the old value in the new one
+			/*YamlConfiguration old = YamlConfiguration.loadConfiguration(backup);
+			YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
+			//Storing line after the comment and the comment itself
+			Map<String, String> comments = new HashMap<String, String>();
+			try{
+				Scanner scanner = new Scanner(new FileInputStream(backup));
+
+				for(int i = 0; scanner.hasNextLine(); i++){
+					String line = scanner.nextLine();
+					if(line.matches("\\s*#.*") || line.matches("^\\s*$")) //If line is a comment or blank
+						comments.put(i, line); //Add it to the map of comment/blank lines
+				}
+				scanner.close();
+			}
+			catch(Exception e3){
+				Messenger.err("Something went wrong while retreiving comments from config. Error:");
+				e3.printStackTrace();
+			}
+
+			//Copy over the keys that are different between the two config files
+			for(String key : old.getKeys(true)){
+				//We don't want to copy over the old config-version or parent nodes
+				if(key.equals("config-version") || old.get(key).toString().startsWith("MemorySection")) continue;
+
+				if(!config.get(key).equals(old.get(key))){//If the old and new values don't match
+					//Set the old value in the new config
+					if(config.contains(key))
+						config.set(key, old.get(key));
+				}
+			}
+			//Save the new config using Bukkit's abysmal YAML implementation
+			try {
+				config.options().header(null);
+				config.save(configFile);
+			} catch (Exception e) {
+				Messenger.err("Unable to copy values from old config to new. Error is: ");
+				e.printStackTrace();
+			}
+			//Re-insert the comments
+			try {
+				Scanner scanner = new Scanner(new FileInputStream(configFile));
+
+				Queue<String> newContents = new LinkedList<String>(); //This will contain the node-lines from the new config
+				while(scanner.hasNextLine())
+					newContents.add(scanner.nextLine());
+				scanner.close();
+
+				String finalContents = ""; //We'll be putting the whole file in this string before writing it out
+
+				//Looping through until we don't have any more values in the queue
+				for(int index = 0; !newContents.isEmpty(); index++){
+					if(comments.containsKey(index)) //If we had a comment on that line in the previous file
+						finalContents += comments.get(index) + "\n"; //Insert it here, then add a next line character
+					else //Otherwise we put the next line from the new config and dequeue it
+						finalContents += newContents.remove() + "\n";
+				}
+
+				//Now we write the string we made to the file
+				Writer fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(configFile)));
+				fileWriter.write(finalContents);
+				fileWriter.close();
+				Messenger.info("Successfully copied values from old config to new");
+			} catch (Exception e1) {
+				Messenger.err("Unable to insert comments from old config to new one. Error is:");
+				e1.printStackTrace();
+			}*/
 		}
 		plugin.saveDefaultConfig();
 	}
@@ -38,7 +115,6 @@ public class OCMConfigHandler {
 		return new File(plugin.getDataFolder() + File.separator + fileName);
 	}
 	public boolean doesConfigymlExist(){
-		File file = getFile("config.yml");
-		return file.exists();
+		return getFile("config.yml").exists();
 	}
 }
