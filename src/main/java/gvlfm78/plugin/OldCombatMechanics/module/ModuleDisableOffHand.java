a5909b88d485d82afa60293b83511411d0644385
kernitus <2789734+kernitus@users.noreply.github.com>
2017-01-18 21:18:13 +0100

    Fixed offhand whitelist

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleDisableOffHand.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleDisableOffHand.java
@@ -17,14 +17,29 @@ import kernitus.plugin.OldCombatMechanics.OCMMain;
 
 public class ModuleDisableOffHand extends Module {
 
+	private static ArrayList<Material> mats = new ArrayList<Material>();
+	
 	public ModuleDisableOffHand(OCMMain plugin) {
 		super(plugin, "disable-offhand");
+		
+		List<String> items = module().getStringList("items");
+
+		//There is no list, just block everything
+		if(items==null || items.isEmpty())
+			return;
+
+		//Looping through name list and adding valid materials to list
+		for(String itemName : items){
+			Material foundMat = Material.matchMaterial(itemName);
+			if(foundMat != null) mats.add(foundMat);
+		}
 	}
 
 	@EventHandler (priority = EventPriority.HIGHEST)
 	public void onSwapHandItems(PlayerSwapHandItemsEvent e) {
-		if(isEnabled(e.getPlayer().getWorld()))
+		if(isEnabled(e.getPlayer().getWorld()) && shouldWeCancel(e.getOffHandItem())){
 			e.setCancelled(true);
+		}
 	}
 
 	@EventHandler (priority = EventPriority.HIGHEST)
@@ -38,8 +53,7 @@ public class ModuleDisableOffHand extends Module {
 		if(!e.getCurrentItem().getType().equals(Material.AIR)) return; 
 		// If the slot is not empty, allow taking the item
 
-		ItemStack item = e.getCursor();
-		if(shouldWeCancel(item)){
+		if(shouldWeCancel(e.getCursor())){
 			e.setResult(Event.Result.DENY);
 			e.setCancelled(true);
 		}
@@ -51,31 +65,18 @@ public class ModuleDisableOffHand extends Module {
 				!e.getInventory().getType().equals(InventoryType.CRAFTING) || 
 				!e.getInventorySlots().contains(40)) return;
 
-		e.setResult(Event.Result.DENY);
-		e.setCancelled(true);
+		if(shouldWeCancel(e.getOldCursor())){
+			e.setResult(Event.Result.DENY);
+			e.setCancelled(true);
+		}
 	}
 
-	public boolean shouldWeCancel(ItemStack item){
-		//Now checking if the item
+	public boolean shouldWeCancel(ItemStack item){		
 		Material mat = item.getType();
-		List<String> items = module().getStringList("items");
-
-		//There is no list, just block everything
-		if(items==null || items.isEmpty())
-			return true;
-
-		//List of materials in list
-		ArrayList<Material> mats = new ArrayList<Material>();
-		//Looping through name list and adding valid materials to list
-		for(String itemName : items){
-			Material foundMat = Material.matchMaterial(itemName);
-			if(foundMat != null) mats.add(foundMat);
-		}
-
 		boolean isContained = mats.contains(mat);
 		boolean isWhitelist = module().getBoolean("whitelist");
 
-		if(isWhitelist && !isContained || !isWhitelist && isContained )
+		if(isWhitelist && !isContained && !mat.equals(Material.AIR) || !isWhitelist && isContained )
 			return true;
 
 		return false;
