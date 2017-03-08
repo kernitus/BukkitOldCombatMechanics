ce1084dd77416ebf6581057ffe27cb70aed8cf6b
gvlfm78 <gvlfm78@users.noreply.github.com>
2017-03-08 20:03:41 +0100

    Fixed assuming bow shooter's entity type

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleDisableBowBoost.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleDisableBowBoost.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.projectiles.ProjectileSource;
 
 import gvlfm78.plugin.OldCombatMechanics.OCMMain;
 
@@ -21,11 +22,17 @@ public class ModuleDisableBowBoost extends Module {
 	public void onProjectileHit(EntityDamageByEntityEvent e){
 		Entity entity = e.getEntity();
 		Entity damager = e.getDamager();
-		if(entity != null && entity instanceof Player && e.getDamager().getType().equals(EntityType.ARROW)){
+		if(entity != null && entity instanceof Player && damager.getType().equals(EntityType.ARROW)){
 			Player player = (Player) entity;
-			if(isEnabled(player.getWorld()) && player.getUniqueId().equals(((Player) ((Arrow) damager).getShooter()).getUniqueId())){
-				e.setCancelled(true);
-				debug("We cancelled your bow boost", player);
+			if(isEnabled(player.getWorld())){
+				ProjectileSource shooter = ((Arrow) damager).getShooter();
+				if(shooter instanceof Player){
+					Player shootingPlayer = (Player) shooter;
+					if(player.getUniqueId().equals(shootingPlayer.getUniqueId())){
+						e.setCancelled(true);
+						debug("We cancelled your bow boost", player);
+					}
+				}
 			}
 		}
 	}
