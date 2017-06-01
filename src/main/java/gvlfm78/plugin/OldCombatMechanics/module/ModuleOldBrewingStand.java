4c12db4c9463795793c1eff681f84acd6ad97d85
gvlfm78 <gvlfm78@users.noreply.github.com>
2017-06-01 20:26:30 +0200

    Fixed brewing stand fuel duplication bugs

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleOldBrewingStand.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleOldBrewingStand.java
@@ -1,8 +1,17 @@
 package gvlfm78.plugin.OldCombatMechanics.module;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BrewingStand;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.inventory.BrewEvent;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
@@ -31,7 +40,10 @@ public class ModuleOldBrewingStand extends Module {
 
 		if(e.getInventory().getType().equals(InventoryType.BREWING)){
 			ItemStack item = e.getCurrentItem();
-			if(item!=null && item.getType().equals(Material.BLAZE_POWDER) && e.getRawSlot()==4)
+
+			if(item!=null && 
+					( (item.getType().equals(Material.BLAZE_POWDER) && e.getRawSlot() == 4) 
+							|| e.getClick().equals(ClickType.DOUBLE_CLICK) ) )
 				e.setCancelled(true);
 		}
 	}
@@ -56,5 +68,30 @@ public class ModuleOldBrewingStand extends Module {
 		Inventory inv = e.getInventory();
 		if(inv.getType().equals(InventoryType.BREWING))
 			( (BrewerInventory) inv).setFuel(new ItemStack(Material.BLAZE_POWDER, 64));
+	}
+
+	@EventHandler (priority = EventPriority.HIGHEST)
+	public void onBlockBreak(BlockBreakEvent e){
+		Block block = e.getBlock();
+		if(!block.getType().equals(Material.BREWING_STAND)) return;
+		//e.setDropItems(false);
+
+		e.setCancelled(true);
+
+		BrewingStand stand = (BrewingStand) block.getState();
+		BrewerInventory inv = stand.getInventory();
+
+		final List<HumanEntity> viewers = inv.getViewers();
+
+		//Making a copy to not get a concurrent modification exception
+		final List<HumanEntity> vws = new ArrayList<HumanEntity>();
+		vws.addAll(viewers);
+		
+		//Closing the inventory of all viewers because this event does strange stuff when this doesn't happen
+		//Also so that above listeners can remove the fule blaze powder before it is dropped
+		vws.forEach(viewer -> viewer.closeInventory());
+
+		//Removing the block
+		block.setType(Material.AIR);
 	}
 }
