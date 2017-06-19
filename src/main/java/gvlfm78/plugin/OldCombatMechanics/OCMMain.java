affe9475d7f281e1727a3bca5cec1034e36b4368
gvlfm78 <gvlfm78@users.noreply.github.com>
2017-06-19 19:14:20 +0200

    Added disable enderpearl cooldown module and so upped config number

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/OCMMain.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/OCMMain.java
@@ -21,6 +21,7 @@ import gvlfm78.plugin.OldCombatMechanics.module.Module;
 import gvlfm78.plugin.OldCombatMechanics.module.ModuleAttackCooldown;
 import gvlfm78.plugin.OldCombatMechanics.module.ModuleDisableBowBoost;
 import gvlfm78.plugin.OldCombatMechanics.module.ModuleDisableElytra;
+import gvlfm78.plugin.OldCombatMechanics.module.ModuleDisableEnderpearlCooldown;
 import gvlfm78.plugin.OldCombatMechanics.module.ModuleDisableOffHand;
 import gvlfm78.plugin.OldCombatMechanics.module.ModuleDisableProjectileRandomness;
 import gvlfm78.plugin.OldCombatMechanics.module.ModuleFishingKnockback;
@@ -156,6 +157,7 @@ public class OCMMain extends JavaPlugin {
 		ModuleLoader.AddModule(new ModuleDisableBowBoost(this));
 		ModuleLoader.AddModule(new ModuleProjectileKnockback(this));
 		ModuleLoader.AddModule(new ModuleNoLapisEnchantments(this));
+		ModuleLoader.AddModule(new ModuleDisableEnderpearlCooldown(this));
 
 		getCommand("OldCombatMechanics").setExecutor(new OCMCommandHandler(this, this.getFile()));// Firing commands listener
 	}
