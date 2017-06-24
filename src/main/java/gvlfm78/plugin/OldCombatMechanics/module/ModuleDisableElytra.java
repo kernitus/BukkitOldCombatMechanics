87ea8d6835907b46fcb9dacd906609c0aef8fadc
kernitus <2789734+kernitus@users.noreply.github.com>
2017-06-24 10:24:51 +0200

    Changed elytra module to not allow wearing instead of destroying item

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleDisableElytra.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleDisableElytra.java
@@ -1,26 +1,75 @@
 package kernitus.plugin.OldCombatMechanics.module;
 
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import kernitus.plugin.OldCombatMechanics.OCMMain;
+import kernitus.plugin.OldCombatMechanics.utilities.Config;
 
 public class ModuleDisableElytra extends Module {
+
+	public static ModuleSwordBlocking INSTANCE;
 
 	public ModuleDisableElytra(OCMMain plugin){
 		super(plugin, "disable-elytra");
 	}
 
-	//Stop players from being able to use elytras
-	@SuppressWarnings("deprecation")
-	@EventHandler
+	@EventHandler (priority = EventPriority.HIGHEST)
 	public void onInventoryClick(InventoryClickEvent e){
 		if(!isEnabled(e.getWhoClicked().getWorld())) return;
-		if(e.getCursor().getType().equals(Material.ELYTRA)){
-			e.setCursor(null); //This deletes the elytra
-			((Player) e.getWhoClicked()).updateInventory(); //This makes sure it doesn't still appear to them as if they have the item
+
+		InventoryType type = e.getInventory().getType(); //Only if they're in their inventory, not chests etc.
+		if(!type.equals(InventoryType.CRAFTING) && !type.equals(InventoryType.PLAYER)) return;
+
+		if(e.getSlot() == 38){
+			e.setCancelled(true);
+			return;
 		}
+
+		if(!e.getCursor().getType().equals(Material.ELYTRA) && !e.getCurrentItem().getType().equals(Material.ELYTRA)) return;
+
+		//Stop shift clicking elytra in
+		ClickType ct = e.getClick();
+		if( !ct.equals(ClickType.SHIFT_LEFT) && !ct.equals(ClickType.SHIFT_RIGHT)) return;
+
+		e.setCancelled(true);
+	}
+
+	@EventHandler (priority = EventPriority.HIGHEST)
+	public void onRightClick(PlayerInteractEvent e){
+
+		if(!isEnabled(e.getPlayer().getWorld())) return;
+
+		//Must not be able to right click while holding it to wear it
+		Action a = e.getAction();
+		if(a != Action.RIGHT_CLICK_AIR && a != Action.RIGHT_CLICK_BLOCK) return;
+
+		if(!e.getMaterial().equals(Material.ELYTRA)) return;
+
+		Block block = e.getClickedBlock();
+		if(block != null && Config.getInteractiveBlocks().contains(block.getType())) return;
+
+		e.setCancelled(true);
+	}
+
+	//Make sure they can't click in, shift click, number key, drag in
+
+	@EventHandler (priority = EventPriority.HIGHEST)
+	public void onDrag(InventoryDragEvent e){
+		if(!isEnabled(e.getWhoClicked().getWorld())) return;
+
+		if(!e.getOldCursor().getType().equals(Material.ELYTRA)) return;
+
+		if(!e.getInventorySlots().contains(38)) return;
+
+		e.setCancelled(true);
 	}
 }
