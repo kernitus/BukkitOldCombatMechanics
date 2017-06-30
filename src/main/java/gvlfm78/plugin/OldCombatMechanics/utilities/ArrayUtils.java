af09f673cf193302cfb263fb55fc14eda1631df6
Rayzr522 <rayzr522@gmail.com>
2017-06-30 10:26:04 -0400

    Cleaned up and documented utilities

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/ArrayUtils.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/ArrayUtils.java
@@ -1,74 +1,59 @@
-
 package gvlfm78.plugin.OldCombatMechanics.utilities;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class ArrayUtils {
-
-	public static String concatArray(Object[] arr, String filler) {
-
-		if (arr.length == 0) {
-
-		return "";
-
-		}
-
-		String output = "";
-		for (int i = 0; i < arr.length - 1; i++) {
-
-			output += arr[i].toString();
-			output += filler;
-
-		}
-
-		output += arr[arr.length - 1].toString();
-
-		return output;
-
-	}
-
-	public static Object[] removeFirst(Object[] arr) {
-
-		if (arr.length <= 1) {
-
-		return arr;
-
-		}
-
-		Object[] out = new Object[arr.length - 1];
-
-		for (int i = 1; i < arr.length; i++) {
-
-			out[i - 1] = arr[i];
-
-		}
-
-		return out;
-
-	}
-
-	public static Object[] removeLast(Object[] arr) {
-
-		if (arr.length <= 1) {
-
-			return arr;
-
-		}
-
-		Object[] out = new Object[arr.length - 1];
-
-		for (int i = 0; i < arr.length - 1; i++) {
-
-			out[i] = arr[i];
-
-		}
-
-		return out;
-
-	}
-
-	public static <T> T last(T[] arr) {
-
-        return arr[arr.length-1];
-
+    /**
+     * Joins the elements of an array with the given filler.
+     *
+     * @param array  The array to concatenate.
+     * @param filler The filler to use.
+     * @return The concatenated string.
+     */
+    public static String concatArray(Object[] array, String filler) {
+        return Arrays.stream(array).map(Objects::toString).collect(Collectors.joining(filler));
     }
 
+    /**
+     * Removes the first item from an array unless it is the only item.
+     *
+     * @param array The array to modify.
+     * @param <T>   The type of the array.
+     * @return The modified array.
+     */
+    public static <T> T[] removeFirst(T[] array) {
+        if (array.length <= 1) {
+            return array;
+        }
+
+        return Arrays.copyOfRange(array, 1, array.length);
+    }
+
+    /**
+     * Removes the last item from an array unless it is the only item.
+     *
+     * @param array The array to modify.
+     * @param <T>   The type of the array.
+     * @return The modified array.
+     */
+    public static <T> T[] removeLast(T[] array) {
+        if (array.length <= 1) {
+            return array;
+        }
+
+        return Arrays.copyOfRange(array, 0, array.length - 1);
+    }
+
+    /**
+     * Safely returns the last element of the array. This will return <code>null</code> if the array length is less than 1.
+     *
+     * @param array The array to retrieve the last element from.
+     * @param <T>   The type of the array.
+     * @return The last element in the array or <code>null</code>.
+     */
+    public static <T> T last(T[] array) {
+        return array.length < 1 ? null : array[array.length - 1];
+    }
 }
--- src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/TextUtils.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/TextUtils.java
@@ -1,26 +1,35 @@
-
 package gvlfm78.plugin.OldCombatMechanics.utilities;
 
 import org.bukkit.ChatColor;
 
 public class TextUtils {
+    /**
+     * Converts ampersand (&) color codes to Minecraft ({@link ChatColor#COLOR_CHAR}) color codes.
+     *
+     * @param text The text to colorize.
+     * @return The colorized text.
+     */
+    public static String colorize(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
 
-	public static String colorize(String text) {
+    /**
+     * Converts Minecraft ({@link ChatColor#COLOR_CHAR}) color codes to ampersand (&) color codes.
+     *
+     * @param text The text to uncolorize.
+     * @return The uncolorized text.
+     */
+    public static String uncolorize(String text) {
+        return text.replace(ChatColor.COLOR_CHAR, '&');
+    }
 
-		return ChatColor.translateAlternateColorCodes('&', text);
-
-	}
-
-	public static String uncolorize(String text) {
-
-		return text.replace(ChatColor.COLOR_CHAR, '&');
-
-	}
-
-	public static String stripColor(String text) {
-
-		return ChatColor.stripColor(text);
-
-	}
-
+    /**
+     * Removes all Minecraft ({@link ChatColor#COLOR_CHAR}) color codes from a string.
+     *
+     * @param text The text to strip colors from.
+     * @return The stripped text.
+     */
+    public static String stripColor(String text) {
+        return ChatColor.stripColor(text);
+    }
 }
