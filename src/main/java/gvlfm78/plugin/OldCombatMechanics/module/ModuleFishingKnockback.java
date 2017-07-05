ad8e03f779c6dadd14bda5fb3271bc786b6b3695
kernitus <2789734+kernitus@users.noreply.github.com>
2017-07-05 11:12:01 +0100

    Fixed minor errors in fishing knockback module

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleFishingKnockback.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleFishingKnockback.java
@@ -1,11 +1,12 @@
 package kernitus.plugin.OldCombatMechanics.module;
 
-import java.util.Collection;
-import java.util.EnumMap;
-
+import com.google.common.base.Functions;
+import com.google.common.collect.ImmutableMap;
+import kernitus.plugin.OldCombatMechanics.OCMMain;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FishHook;
@@ -18,10 +19,8 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.plugin.RegisteredListener;
 
-import com.google.common.base.Functions;
-import com.google.common.collect.ImmutableMap;
-
-import kernitus.plugin.OldCombatMechanics.OCMMain;
+import java.util.Collection;
+import java.util.EnumMap;
 
 /**
  * Created by Rayzr522 on 6/27/16.
@@ -35,8 +34,10 @@ public class ModuleFishingKnockback extends Module {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onRodLand(ProjectileHitEvent e) {
 
-		if (!isEnabled(e.getEntity().getWorld()))
-			return;
+		Entity hookEntity = e.getEntity();
+		World world = hookEntity.getWorld();
+
+		if (!isEnabled(world)) return;
 
 		if (e.getEntityType() != EntityType.FISHING_HOOK)
 			return;
@@ -48,32 +49,32 @@ public class ModuleFishingKnockback extends Module {
 			hitent = e.getHitEntity();
 		}
 		catch(NoSuchMethodError e1){ //For older version that don't have such method
-			Collection<Entity> entities = e.getEntity().getLocation().getWorld().getNearbyEntities(e.getEntity().getLocation(), 0.25, 0.25, 0.25);
+			Collection<Entity> entities = world.getNearbyEntities(hookEntity.getLocation(), 0.25, 0.25, 0.25);
 
 			for (Entity entity : entities) {
-				if (entity instanceof Player){
+				if (entity instanceof Player)
 					hitent = entity;
 					break;
-				}
+
 			}
 		}
 
 		if(hitent == null) return;
 		if(!(hitent instanceof Player)) return;
 
-		FishHook hook = (FishHook) e.getEntity();
+		FishHook hook = (FishHook) hookEntity;
 		Player rodder = (Player) hook.getShooter();
 		Player player = (Player) hitent;
 
 		debug("You were hit by a fishing rod!", player);
 
-		if (player.getUniqueId() == rodder.getUniqueId())
+		if (player.getUniqueId().equals(rodder.getUniqueId()))
 			return;
 
 		if(player.getGameMode() == GameMode.CREATIVE) return;
 
 		double damage = module().getDouble("damage");
-		if(damage<0) damage = 0.2;
+		if(damage < 0) damage = 0.2;
 
 		EntityDamageEvent event = makeEvent(rodder, player, damage);
 		Bukkit.getPluginManager().callEvent(event);
