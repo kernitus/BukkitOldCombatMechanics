03b6e9a39b92f3fa8dad6e728313f2ac1e44d557
kernitus <2789734+kernitus@users.noreply.github.com>
2017-07-28 15:31:37 +0200

    Made shield blocking uninterrupted, added config for shield damage reduction and other options for new blocking method

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModulePlayerCollisions.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModulePlayerCollisions.java
@@ -3,6 +3,7 @@ package kernitus.plugin.OldCombatMechanics.module;
 import kernitus.plugin.OldCombatMechanics.OCMMain;
 import kernitus.plugin.OldCombatMechanics.utilities.reflection.TeamUtils;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -15,13 +16,12 @@ public class ModulePlayerCollisions extends Module {
 
 	}
 
-	@EventHandler
+	@EventHandler (priority = EventPriority.MONITOR)
 	public void onPlayerLogin(PlayerJoinEvent e) {
-		e.getPlayer().sendMessage("You logged in");
 		TeamUtils.sendTeamPacket(e.getPlayer());
 	}
 
-	@EventHandler
+	@EventHandler (priority = EventPriority.MONITOR)
 	public void onPlayerQuit(PlayerQuitEvent e){
 		TeamUtils.getSecurePlayers().remove(e.getPlayer());
 	}
