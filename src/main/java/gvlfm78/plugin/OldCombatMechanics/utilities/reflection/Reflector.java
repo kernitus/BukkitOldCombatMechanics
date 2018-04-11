e62b5daca5287d18edf901c74b2a878c1a3b60a9
Rayzr522 <rayzr522@gmail.com>
2018-04-10 22:22:12 -0400

    Removed extra reflection code

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/reflection/Reflector.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/reflection/Reflector.java
@@ -1,11 +1,9 @@
 package gvlfm78.plugin.OldCombatMechanics.utilities.reflection;
 
 import gvlfm78.plugin.OldCombatMechanics.utilities.reflection.type.ClassType;
-import gvlfm78.plugin.OldCombatMechanics.utilities.reflection.type.HandleType;
 import gvlfm78.plugin.OldCombatMechanics.utilities.reflection.type.PacketType;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -18,32 +16,17 @@ import java.util.Arrays;
  */
 
 public class Reflector {
-
-    public static Class<?> CRAFT_PLAYER;
-    public static Class<?> CRAFT_ENTITY;
-    public static Class<?> CRAFT_SERVER;
-    public static Class<?> CRAFT_WORLD;
-    public static Class<?> CRAFT_ITEM_STACK;
-    private static String version = "";
     private static Method PLAYER_HANDLE;
-    private static Method ENTITY_HANDLE;
-    private static Method SERVER_HANDLE;
-    private static Method WORLD_HANDLE;
+    private static String version = "";
 
     static{
         try{
             version = Bukkit.getServer().getClass().getName().split("\\.")[3];
 
-            CRAFT_PLAYER = getClass(ClassType.CRAFTBUKKIT, "entity.CraftPlayer");
-            CRAFT_ENTITY = getClass(ClassType.CRAFTBUKKIT, "entity.CraftEntity");
-            CRAFT_SERVER = getClass(ClassType.CRAFTBUKKIT, "CraftServer");
-            CRAFT_WORLD = getClass(ClassType.CRAFTBUKKIT, "CraftWorld");
-            CRAFT_ITEM_STACK = getClass(ClassType.CRAFTBUKKIT, "inventory.CraftItemStack");
+            Class<?> CRAFT_PLAYER = getClass(ClassType.CRAFTBUKKIT, "entity.CraftPlayer");
+            assert CRAFT_PLAYER != null;
 
             PLAYER_HANDLE = getMethod(CRAFT_PLAYER, "getHandle");
-            ENTITY_HANDLE = getMethod(CRAFT_ENTITY, "getHandle");
-            SERVER_HANDLE = getMethod(CRAFT_SERVER, "getHandle");
-            WORLD_HANDLE = getMethod(CRAFT_WORLD, "getHandle");
 
         } catch(Exception e){
             System.err.println("Failed to load Reflector");
@@ -102,30 +85,12 @@ public class Reflector {
                 .orElse(null);
     }
 
-    public static Object getHandle(HandleType type, Object object){
-        try{
-            switch(type){
-                case ENTITY:
-                    return ENTITY_HANDLE.invoke(object);
-                case PLAYER:
-                    return PLAYER_HANDLE.invoke(object);
-                case SERVER:
-                    return SERVER_HANDLE.invoke(object);
-                case WORLD:
-                    return WORLD_HANDLE.invoke(object);
-                default:
-                    return null;
-            }
 
-        } catch(Exception e){
-            return null;
-        }
-    }
-
-    public static Object getNMSItem(ItemStack item){
+    private static Object getPlayerHandler(Player player){
         try{
-            return getFieldValue(item, "handle");
-        } catch(Exception e){
+            return PLAYER_HANDLE.invoke(player);
+        } catch(InvocationTargetException | IllegalAccessException e){
+            e.printStackTrace();
             return null;
         }
     }
@@ -137,7 +102,7 @@ public class Reflector {
 
         public static void sendPacket(Player player, Object packet){
             try{
-                Object nmsPlayer = getHandle(HandleType.PLAYER, player);
+                Object nmsPlayer = getPlayerHandler(player);
                 assert nmsPlayer != null;
 
                 Object con = getFieldValue(nmsPlayer, "playerConnection");
--- src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/reflection/type/HandleType.java
+++
@@ -1,7 +0,0 @@
-package gvlfm78.plugin.OldCombatMechanics.utilities.reflection.type;
-
-public enum HandleType {
-
-    PLAYER, ENTITY, SERVER, WORLD
-
-}
