3e9fdfc9eef9a0c0dd708f8eae3146090af4c464
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-05-06 19:03:31 +0200

    Make the collision per-world

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModulePlayerCollisions.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModulePlayerCollisions.java
@@ -9,6 +9,7 @@ import kernitus.plugin.OldCombatMechanics.utilities.reflection.TeamUtils;
 import kernitus.plugin.OldCombatMechanics.utilities.reflection.type.ClassType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -22,8 +23,21 @@ public class ModulePlayerCollisions extends Module {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerLogin(PlayerJoinEvent e) {
-        TeamUtils.sendTeamPacket(e.getPlayer());
+        if (isEnabled(e.getPlayer().getWorld())) {
+            TeamUtils.sendTeamPacket(e.getPlayer());
+        }
+
+        // always attach the listener, it checks internally
         PacketManager.getInstance().addListener(collisionPacketListener, e.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerChangeWorld(PlayerChangedWorldEvent e) {
+        if (isEnabled(e.getPlayer().getWorld())) {
+            TeamUtils.sendTeamPacket(e.getPlayer());
+        } else {
+            TeamUtils.sendTeamRemovePacket(e.getPlayer());
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -31,14 +45,18 @@ public class ModulePlayerCollisions extends Module {
         TeamUtils.getSecurePlayers().remove(e.getPlayer());
     }
 
-    private static class CollisionPacketListener extends PacketAdapter {
+    private class CollisionPacketListener extends PacketAdapter {
 
-        private static final Class<?> targetClass = Reflector
+        private final Class<?> targetClass = Reflector
                 .getClass(ClassType.NMS, "PacketPlayOutScoreboardTeam");
 
         @Override
         public void onPacketSend(PacketEvent packetEvent) {
             if (packetEvent.getPacket().getPacketClass() != targetClass) {
+                return;
+            }
+
+            if (!isEnabled(packetEvent.getPlayer().getWorld())) {
                 return;
             }
 
