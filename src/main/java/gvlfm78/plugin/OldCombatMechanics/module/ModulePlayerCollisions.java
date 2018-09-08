05f7dbace4476594e941ccd1d3812cf437cde2cd
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-09-08 13:11:52 +0200

    Rework player collisions

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModulePlayerCollisions.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModulePlayerCollisions.java
@@ -1,23 +1,32 @@
 package kernitus.plugin.OldCombatMechanics.module;
 
 import kernitus.plugin.OldCombatMechanics.OCMMain;
+import kernitus.plugin.OldCombatMechanics.utilities.Messenger;
 import kernitus.plugin.OldCombatMechanics.utilities.packet.PacketAdapter;
 import kernitus.plugin.OldCombatMechanics.utilities.packet.PacketEvent;
 import kernitus.plugin.OldCombatMechanics.utilities.packet.PacketManager;
 import kernitus.plugin.OldCombatMechanics.utilities.reflection.Reflector;
-import kernitus.plugin.OldCombatMechanics.utilities.reflection.TeamUtils;
 import kernitus.plugin.OldCombatMechanics.utilities.reflection.type.ClassType;
+import kernitus.plugin.OldCombatMechanics.utilities.teams.CollisionRule;
+import kernitus.plugin.OldCombatMechanics.utilities.teams.TeamAction;
+import kernitus.plugin.OldCombatMechanics.utilities.teams.TeamPacket;
+import kernitus.plugin.OldCombatMechanics.utilities.teams.TeamUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 public class ModulePlayerCollisions extends Module {
 
     private CollisionPacketListener collisionPacketListener = new CollisionPacketListener();
+    private Map<Player, TeamPacket> playerTeamMap = Collections.synchronizedMap(new WeakHashMap<>());
+
 
     public ModulePlayerCollisions(OCMMain plugin){
         super(plugin, "disable-player-collisions");
@@ -32,28 +41,58 @@ public class ModulePlayerCollisions extends Module {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerLogin(PlayerJoinEvent e){
-        if(isEnabled(e.getPlayer().getWorld())){
-            TeamUtils.sendTeamPacket(e.getPlayer());
-        }
-
         // always attach the listener, it checks internally
         PacketManager.getInstance().addListener(collisionPacketListener, e.getPlayer());
+
+        createOrUpdateTeam(e.getPlayer());
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerChangeWorld(PlayerChangedWorldEvent e){
-        if(isEnabled(e.getPlayer().getWorld())){
-            TeamUtils.sendTeamPacket(e.getPlayer());
+        createOrUpdateTeam(e.getPlayer());
+    }
+
+    /**
+     * Creates a team or updates it by sending the correct packet to the player.
+     *
+     * @param player the player to send it to
+     */
+    private void createOrUpdateTeam(Player player){
+        CollisionRule collisionRule = isEnabled(player.getWorld())
+                ? CollisionRule.NEVER
+                : CollisionRule.ALWAYS;
+
+        if(playerTeamMap.containsKey(player)){
+            TeamPacket teamPacket = playerTeamMap.get(player);
+            teamPacket.setTeamAction(TeamAction.UPDATE);
+            teamPacket.setCollisionRule(collisionRule);
+            teamPacket.send(player);
         } else {
-            if(module().getBoolean("enableTeamDisbanding", true)){
-                TeamUtils.sendTeamRemovePacket(e.getPlayer());
-            }
+            createAndSendNewTeam(player, collisionRule);
         }
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerQuit(PlayerQuitEvent e){
-        TeamUtils.getSecurePlayers().remove(e.getPlayer());
+    /**
+     * Creates a new {@link TeamPacket} stores it in the cache map and sends it to the player.
+     *
+     * @param player        the player to send it to
+     * @param collisionRule the {@link CollisionRule} to use
+     */
+    private void createAndSendNewTeam(Player player, CollisionRule collisionRule){
+        TeamPacket newTeamPacket = TeamUtils.craftTeamCreatePacket(player, collisionRule);
+        playerTeamMap.put(player, newTeamPacket);
+
+        newTeamPacket.send(player);
+    }
+
+
+    @Override
+    public void reload(){
+        super.reload();
+
+        for(Player player : Bukkit.getOnlinePlayers()){
+            createOrUpdateTeam(player);
+        }
     }
 
     private class CollisionPacketListener extends PacketAdapter {
@@ -71,11 +110,45 @@ public class ModulePlayerCollisions extends Module {
                 return;
             }
 
-            try{
-                // this is very prone to changes as it just *gets the field by name*. But not in my code.
-                TeamUtils.changePacketCollisionType(packetEvent.getPacket().getNMSPacket());
-            } catch(ReflectiveOperationException e){
-                throw new RuntimeException("An error occurred setting the collision rule", e);
+            Object nmsPacket = packetEvent.getPacket().getNMSPacket();
+
+            CollisionRule collisionRule = isEnabled(packetEvent.getPlayer().getWorld())
+                    ? CollisionRule.NEVER
+                    : CollisionRule.ALWAYS;
+
+            updateToPacket(
+                    packetEvent.getPlayer(),
+                    playerTeamMap.computeIfAbsent(packetEvent.getPlayer(), player -> new TeamPacket()),
+                    nmsPacket,
+                    collisionRule
+            );
+
+            // Also update the team when a player was added/removed
+            if(TeamUtils.targetsPlayer(nmsPacket, packetEvent.getPlayer())){
+                updateToPacket(
+                        packetEvent.getPlayer(), playerTeamMap.get(packetEvent.getPlayer()), nmsPacket, collisionRule
+                );
+            }
+
+            Messenger.debug(
+                    "Collision rule is %s for action %s in world %sA",
+                    collisionRule,
+                    TeamUtils.getPacketAction(nmsPacket),
+                    packetEvent.getPlayer().getWorld().getName()
+            );
+
+            TeamUtils.setCollisionRule(nmsPacket, collisionRule);
+        }
+
+        /**
+         * Updates the given {@link TeamPacket} to the NMS packet and removes it from the cache, if it was disbanded.
+         */
+        private void updateToPacket(Player player, TeamPacket teamPacket, Object nmsPacket, CollisionRule collisionRule){
+            teamPacket.adjustToUpdate(nmsPacket);
+
+            if(!teamPacket.teamExists()){
+                debug("Recreated team due to disband", player);
+                createAndSendNewTeam(player, collisionRule);
             }
         }
     }
---
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/teams/TeamUtils.java
@@ -0,0 +1,190 @@
+package kernitus.plugin.OldCombatMechanics.utilities.teams;
+
+import kernitus.plugin.OldCombatMechanics.utilities.reflection.Reflector;
+import kernitus.plugin.OldCombatMechanics.utilities.reflection.type.ClassType;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+/**
+ * Contains useful methods when dealing with minecraft scoreboard teams.
+ */
+public class TeamUtils {
+
+    private static final VersionData VERSION_DATA = new VersionData();
+
+    /**
+     * Sets the collision rule of the packet.
+     *
+     * @param packet        the packet to set it for
+     * @param collisionRule the collision rule
+     */
+    public static void setCollisionRule(Object packet, CollisionRule collisionRule){
+        try{
+            VERSION_DATA.getFieldCollisionRule().set(packet, collisionRule.getName());
+        } catch(IllegalAccessException e){
+            // should not happen if the getting works
+            throw new RuntimeException("Error setting collision field", e);
+        }
+    }
+
+    /**
+     * Returns the action of a given packet.
+     *
+     * @param packet the packet
+     * @return the action for the packet
+     */
+    public static TeamAction getPacketAction(Object packet){
+        try{
+            return TeamAction.fromId((Integer) VERSION_DATA.getFieldAction().get(packet));
+        } catch(IllegalAccessException e){
+            // should not happen if the getting works
+            throw new RuntimeException("Error getting action field", e);
+        }
+    }
+
+    /**
+     * Sets the action of a given packet.
+     *
+     * @param packet the packet
+     * @param action the new action
+     */
+    public static void setPacketAction(Object packet, TeamAction action){
+        try{
+            VERSION_DATA.getFieldAction().set(packet, action.getMinecraftId());
+        } catch(IllegalAccessException e){
+            // should not happen if the getting works
+            throw new RuntimeException("Error setting action field", e);
+        }
+    }
+
+    /**
+     * Checks if a team packet targets a player.
+     *
+     * @param packet the packet in question
+     * @param player the player to check for
+     * @return true if the packet targets the player
+     */
+    public static boolean targetsPlayer(Object packet, Player player){
+        return getTeamMembers(packet).contains(player.getName());
+    }
+
+    /**
+     * Creates a new team packet and sends it to the player. The team will have a random name.
+     *
+     * @param player        the player to send it to
+     * @param collisionRule the collision rule to use
+     */
+    public static TeamPacket craftTeamCreatePacket(Player player, CollisionRule collisionRule){
+        String teamName = UUID.randomUUID().toString().substring(0, 15);
+
+        Object teamPacket = VERSION_DATA.createTeamPacket(
+                TeamAction.CREATE,
+                collisionRule,
+                teamName,
+                Collections.singletonList(player)
+        );
+
+        return new TeamPacket(teamPacket);
+    }
+
+    /**
+     * Returns all team members in the given packet.
+     *
+     * @param packet the packet
+     * @return returns all team members
+     */
+    static Collection<String> getTeamMembers(Object packet){
+        try{
+            @SuppressWarnings("unchecked")
+            Collection<String> identifiers = (Collection<String>) VERSION_DATA.getFieldPlayerNames().get(packet);
+
+            return identifiers;
+        } catch(IllegalAccessException e){
+            // should not happen if the getting works
+            throw new RuntimeException("Error getting players", e);
+        }
+    }
+
+    /**
+     * Sets the team members.
+     *
+     * @param packet     the packet
+     * @param newMembers the new team members
+     */
+    static void setTeamMembers(Object packet, Collection<?> newMembers){
+        try{
+            VERSION_DATA.getFieldPlayerNames().set(packet, newMembers);
+        } catch(IllegalAccessException e){
+            throw new RuntimeException("Error setting members", e);
+        }
+    }
+
+    /**
+     * Creates a new team packet with the given data
+     *
+     * @param action        the action to use for the new packet
+     * @param collisionRule the collision rule to use
+     * @param name          the team name
+     * @param players       the players onm the team
+     */
+    static Object createTeamNMSPacket(TeamAction action, CollisionRule collisionRule, String name,
+                                      Collection<Player> players){
+        return VERSION_DATA.createTeamPacket(action, collisionRule, name, players);
+    }
+
+    /**
+     * Contains version specific data. Currently it is just static because the structure of the packet hasn't
+     * changed yet.
+     */
+    private static class VersionData {
+        private final Field fieldCollisionRule;
+        private final Field fieldPlayerNames;
+        private final Field fieldAction;
+        private final Field fieldName;
+        private final Constructor<?> constructorTeamPacket;
+
+        VersionData(){
+            Class<?> packetClass = Reflector.getClass(ClassType.NMS, "PacketPlayOutScoreboardTeam");
+            this.fieldCollisionRule = Reflector.getInaccessibleField(packetClass, "f");
+            this.fieldPlayerNames = Reflector.getInaccessibleField(packetClass, "h");
+            this.fieldAction = Reflector.getInaccessibleField(packetClass, "i");
+            this.fieldName = Reflector.getInaccessibleField(packetClass, "a");
+
+            this.constructorTeamPacket = Reflector.getConstructor(packetClass, 0);
+        }
+
+        Field getFieldCollisionRule(){
+            return fieldCollisionRule;
+        }
+
+        Field getFieldPlayerNames(){
+            return fieldPlayerNames;
+        }
+
+        Field getFieldAction(){
+            return fieldAction;
+        }
+
+        Object createTeamPacket(TeamAction action, CollisionRule collisionRule, String name,
+                                Collection<Player> players){
+            try{
+                Object packet = constructorTeamPacket.newInstance();
+
+                getFieldPlayerNames().set(packet, players.stream().map(Player::getName).collect(Collectors.toList()));
+                getFieldCollisionRule().set(packet, collisionRule.getName());
+                getFieldAction().set(packet, action.getMinecraftId());
+                fieldName.set(packet, name);
+
+                return packet;
+            } catch(ReflectiveOperationException e){
+                throw new RuntimeException("Error creating team packet", e);
+            }
+        }
+    }
+}
