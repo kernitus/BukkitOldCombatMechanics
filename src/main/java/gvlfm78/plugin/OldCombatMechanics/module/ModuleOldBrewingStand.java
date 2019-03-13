e8af186282ccb356fa136d68a29e0a3f49818e86
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-03-13 11:03:23 +0100

    Consolidate refueling logic

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleOldBrewingStand.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleOldBrewingStand.java
@@ -2,14 +2,17 @@ package kernitus.plugin.OldCombatMechanics.module;
 
 import kernitus.plugin.OldCombatMechanics.OCMMain;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.BrewingStand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.BrewEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 
+/**
+ * Makes brewing stands not require fuel.
+ */
 public class ModuleOldBrewingStand extends Module {
 
     public ModuleOldBrewingStand(OCMMain plugin){
@@ -20,13 +23,28 @@ public class ModuleOldBrewingStand extends Module {
     public void onBrew(BrewEvent e){
         Block block = e.getBlock();
 
-        if(isEnabled(block.getWorld()) && block.getType().equals(Material.BREWING_STAND)) //Just in case...
-            ((BrewingStand) block.getState()).setFuelLevel(20);
+        if(!isEnabled(block.getWorld()))
+            return;
+
+        BlockState blockState = block.getState();
+
+        refuel(blockState);
+    }
+
+    private void refuel(BlockState blockState){
+        if(!(blockState instanceof BrewingStand))
+            return;
+
+        BrewingStand brewingStand = (BrewingStand) blockState;
+
+        brewingStand.setFuelLevel(20);
+        brewingStand.update();
     }
 
     @EventHandler
     public void onInventoryOpen(InventoryOpenEvent e){
-        if(!isEnabled(e.getPlayer().getWorld())) return;
+        if(!isEnabled(e.getPlayer().getWorld()))
+            return;
 
         Inventory inv = e.getInventory();
 
@@ -34,6 +52,7 @@ public class ModuleOldBrewingStand extends Module {
 
         Location loc = null;
 
+        // TODO: Why is this needed? It should just return null
         try{
             loc = inv.getLocation();
         } catch(Exception ignored){
@@ -43,64 +62,6 @@ public class ModuleOldBrewingStand extends Module {
 
         Block block = loc.getBlock();
 
-        if(!block.getType().equals(Material.BREWING_STAND)) return;
-
-        BrewingStand stand = (BrewingStand) block.getState();
-
-        stand.setFuelLevel(20);
-
-        stand.update();
-
+        refuel(block.getState());
     }
-
-	/*@EventHandler
-	public void onInventoryClick(InventoryClickEvent e) {
-		if(!isEnabled(e.getWhoClicked().getWorld())) return;
-
-		if(e.getInventory().getType().equals(InventoryType.BREWING)){
-			ItemStack item = e.getCurrentItem();
-
-			if(item!=null && 
-					( (item.getType().equals(Material.BLAZE_POWDER) && e.getRawSlot() == 4) ||
-							( e.getCursor().getType().equals(Material.BLAZE_POWDER) && e.getClick().equals(ClickType.DOUBLE_CLICK) ) ))
-				e.setCancelled(true);
-		}
-	}
-
-	@EventHandler
-	public void onInventoryClose(InventoryCloseEvent e) {
-		if(!isEnabled(e.getPlayer().getWorld())) return;
-
-		Inventory inv = e.getInventory();
-		if(inv.getType().equals(InventoryType.BREWING)){
-			BrewerInventory bi = (BrewerInventory) inv;
-			ItemStack fuel = bi.getFuel();
-			if(fuel != null && fuel.getType().equals(Material.BLAZE_POWDER))
-				bi.setFuel(new ItemStack(Material.AIR));
-		}
-	}
-	@EventHandler (priority = EventPriority.HIGHEST)
-	public void onBlockBreak(BlockBreakEvent e){
-		Block block = e.getBlock();
-		if(!block.getType().equals(Material.BREWING_STAND)) return;
-		//e.setDropItems(false);
-
-		e.setCancelled(true);
-
-		BrewingStand stand = (BrewingStand) block.getState();
-		BrewerInventory inv = stand.getInventory();
-
-		final List<HumanEntity> viewers = inv.getViewers();
-
-		//Making a copy to not get a concurrent modification exception
-		final List<HumanEntity> vws = new ArrayList<HumanEntity>();
-		vws.addAll(viewers);
-
-		//Closing the inventory of all viewers because this event does strange stuff when this doesn't happen
-		//Also so that above listeners can remove the fule blaze powder before it is dropped
-		vws.forEach(viewer -> viewer.closeInventory());
-
-		//Removing the block
-		block.setType(Material.AIR);
-	}*/
 }
