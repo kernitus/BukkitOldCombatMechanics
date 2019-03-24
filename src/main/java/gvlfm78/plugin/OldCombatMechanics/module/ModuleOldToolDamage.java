0eebc1184302249939a71b53dcc5f944bc3ba005
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-03-24 17:35:54 +0100

    Add javadoc, slight refactor

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleOldToolDamage.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/module/ModuleOldToolDamage.java
@@ -11,15 +11,18 @@ import org.bukkit.event.EventHandler;
 
 import java.util.Locale;
 
+/**
+ * Restores old tool damage.
+ */
 public class ModuleOldToolDamage extends Module {
 
-    private String[] weapons = {"sword", "axe", "pickaxe", "spade", "hoe"};
+    private static final String[] WEAPONS = {"sword", "axe", "pickaxe", "spade", "hoe"};
 
     public ModuleOldToolDamage(OCMMain plugin){
         super(plugin, "old-tool-damage");
     }
 
-    @EventHandler (ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true)
     public void onEntityDamaged(OCMEntityDamageByEntityEvent event){
         Entity damager = event.getDamager();
 
@@ -27,33 +30,33 @@ public class ModuleOldToolDamage extends Module {
 
         if(!isEnabled(world)) return;
 
-        Material mat = event.getWeapon().getType();
+        Material weaponMaterial = event.getWeapon().getType();
 
-        if(!isHolding(mat, weapons)) return;
+        if(!isTool(weaponMaterial)) return;
 
-        double weaponDamage = WeaponDamages.getDamage(mat);
+        double weaponDamage = WeaponDamages.getDamage(weaponMaterial);
         if(weaponDamage <= 0) weaponDamage = 1;
 
-        double baseDamage = event.getBaseDamage();
+        double oldBaseDamage = event.getBaseDamage();
 
         event.setBaseDamage(weaponDamage);
-        debug("Old tool damage: " + baseDamage + " New tool damage: " + weaponDamage, damager);
+        debug("Old tool damage: " + oldBaseDamage + " New tool damage: " + weaponDamage, damager);
 
-        //Set sharpness to 1.8 damage value
+        // Set sharpness to 1.8 damage value
         double newSharpnessDamage = DamageUtils.getOldSharpnessDamage(event.getSharpnessLevel());
         debug("Old sharpness damage: " + event.getSharpnessDamage() + " New: " + newSharpnessDamage, damager);
         event.setSharpnessDamage(newSharpnessDamage);
     }
 
-    private boolean isHolding(Material mat, String type){
-        return mat.toString().endsWith("_" + type.toUpperCase(Locale.ROOT));
+    private boolean isTool(Material material){
+        for(String type : WEAPONS)
+            if(isOfType(material, type))
+                return true;
+
+        return false;
     }
 
-    private boolean isHolding(Material mat, String[] types){
-        boolean hasAny = false;
-        for(String type : types)
-            if(isHolding(mat, type))
-                hasAny = true;
-        return hasAny;
+    private boolean isOfType(Material mat, String type){
+        return mat.toString().endsWith("_" + type.toUpperCase(Locale.ROOT));
     }
 }
