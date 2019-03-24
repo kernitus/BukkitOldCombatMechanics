4a24b0eb82efc22ba3d258b9aa570af9ddd3975c
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-03-24 17:14:39 +0100

    Add javadoc and slight refactor

--- src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/potions/GenericPotionDurations.java
+++ src/main/java/gvlfm78/plugin/OldCombatMechanics/utilities/potions/GenericPotionDurations.java
@@ -1,7 +1,9 @@
 package gvlfm78.plugin.OldCombatMechanics.utilities.potions;
 
 /**
- * Used to hold information on duration of base, II & extended versions of potion
+ * Used to hold information on duration of base, II & extended versions of potion.
+ * <p>
+ * All durations are in seconds.
  */
 public class GenericPotionDurations {
 
@@ -13,14 +15,29 @@ public class GenericPotionDurations {
         this.extended = extended;
     }
 
+    /**
+     * Returns the base potion duration in seconds.
+     *
+     * @return the base potion duration in seconds.
+     */
     public int getBaseTime(){
         return base;
     }
 
+    /**
+     * Returns the duration in seconds for the amplified potion.
+     *
+     * @return the duration in seconds for the amplified potion.
+     */
     public int getIITime(){
         return II;
     }
 
+    /**
+     * Returns the duration in seconds for the extended potion.
+     *
+     * @return the duration in seconds for the extended potion
+     */
     public int getExtendedTime(){
         return extended;
     }
