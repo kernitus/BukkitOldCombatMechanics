ef1503d7585f64114f5360e8552dd8e231a06654
Shanyu Juneja <shanyujuneja@gmail.com>
2020-03-30 13:49:24 +0200

    Added config bit, added velocity modifiers to arrows

--- src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleOldArrows.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleOldArrows.java
@@ -6,7 +6,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.projectiles.ProjectileSource;
+import org.bukkit.util.Vector;
 
 /**
  * Prevents players using new arrow mechanics
@@ -36,9 +38,30 @@ public class ModuleOldArrows extends Module {
 
         Arrow arrow = (Arrow) e.getDamager();
 
-        if (arrow.hasCustomEffects()){
+        if (arrow.hasCustomEffects() && module().getBoolean("clear-effects")){
             arrow.clearCustomEffects();
         }
-
+        e.setDamage(arrow.getDamage() * module().getDouble("damage-multiplier"));
     }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onProjectileLaunch(ProjectileLaunchEvent e){
+        if(!(e.getEntity() instanceof Arrow)){
+            return;
+        }
+
+        Arrow arrow = (Arrow) e.getEntity();
+
+        Vector velocity = arrow.getVelocity();
+        double horizontalModifier = module().getDouble("velocity-multiplier.horizontal");
+        double verticalModifier = module().getDouble("velocity-multiplier.vertical");
+        Vector velocityModified = new Vector(
+                horizontalModifier * velocity.getX(),
+                verticalModifier * velocity.getY(),
+                horizontalModifier * velocity.getZ()
+        );
+
+        arrow.setVelocity(velocityModified);
+    }
+
 }
