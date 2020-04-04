30964b509633cbab2df2ead265e272c96d77b641
Koem <kosmi.bartek@gmail.com>
2020-04-04 16:12:48 +0200

    fixed bug: no damage with egg, snowball projectiles under Absorption effect #361
    updated bstats-bukkit

--- src/main/java/kernitus/plugin/OldCombatMechanics/OCMMain.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/OCMMain.java
@@ -3,6 +3,7 @@ package kernitus.plugin.OldCombatMechanics;
 import kernitus.plugin.OldCombatMechanics.hooks.PlaceholderAPIHook;
 import kernitus.plugin.OldCombatMechanics.hooks.api.Hook;
 import kernitus.plugin.OldCombatMechanics.module.*;
+import kernitus.plugin.OldCombatMechanics.module.Module;
 import kernitus.plugin.OldCombatMechanics.updater.ModuleUpdateChecker;
 import kernitus.plugin.OldCombatMechanics.utilities.Config;
 import kernitus.plugin.OldCombatMechanics.utilities.Messenger;
@@ -79,7 +80,7 @@ public class OCMMain extends JavaPlugin {
         }
 
         //BStats Metrics
-        Metrics metrics = new Metrics(this);
+        Metrics metrics = new Metrics(this, 0);
 
 
         /*Custom bar charts currently disabled on bStats
--- src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleProjectileKnockback.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleProjectileKnockback.java
@@ -1,32 +1,54 @@
 package kernitus.plugin.OldCombatMechanics.module;
 
 import kernitus.plugin.OldCombatMechanics.OCMMain;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.util.Vector;
 
 import java.util.Locale;
+import java.util.Random;
 
 /**
  * Adds knockback to eggs, snowballs and ender pearls.
  */
 public class ModuleProjectileKnockback extends Module {
 
-    public ModuleProjectileKnockback(OCMMain plugin){
+    public ModuleProjectileKnockback(OCMMain plugin) {
         super(plugin, "projectile-knockback");
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onEntityHit(EntityDamageByEntityEvent e){
-        if(!isEnabled(e.getEntity().getWorld())) return;
+    public void onEntityHit(EntityDamageByEntityEvent e) {
+        if (!isEnabled(e.getEntity().getWorld())) {
+            return;
+        }
 
         EntityType type = e.getDamager().getType();
+        final Entity attacker = e.getDamager();
+        final Entity victim = e.getEntity();
 
-        switch(type){
+        switch (type) {
             case SNOWBALL:
             case EGG:
             case ENDER_PEARL:
+                if (victim instanceof LivingEntity) {
+                    LivingEntity livingVictim = ((LivingEntity) victim);
+                    if (livingVictim.getAbsorptionAmount() > 0) {
+                        livingVictim.damage(module().getDouble("damage." + type.toString().toLowerCase(Locale.ROOT)), attacker);
+
+                        double randomValue = 0.35 + (0.1) * new Random().nextDouble();
+
+                        // mocking Minecraft knockback calculation
+                        final Vector knockBack = victim.getLocation().toVector().subtract(attacker.getLocation().toVector()).normalize().multiply(randomValue).setY(0.4);
+                        victim.setVelocity(knockBack);
+                        e.setCancelled(true);
+                        break;
+                    }
+                }
                 e.setDamage(module().getDouble("damage." + type.toString().toLowerCase(Locale.ROOT)));
             default:
                 break;
