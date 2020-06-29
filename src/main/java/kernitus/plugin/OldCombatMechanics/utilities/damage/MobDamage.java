671e0cfd34d5fefc3b617ac9d045290dbb25a00c
kernitus <2789734+kernitus@users.noreply.github.com>
2020-06-29 14:58:19 +0200

    Updated Spigot dep. & list of undead & arthropods

--- src/main/java/kernitus/plugin/OldCombatMechanics/utilities/damage/MobDamage.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/utilities/damage/MobDamage.java
@@ -7,34 +7,39 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.Map;
 
-/**
- * Created by Rayzr522 on 6/21/16.
- */
 public class MobDamage {
 
-    private static Map<EntityType, Enchantment> enchants = ImmutableMap.<EntityType, Enchantment>builder()
-            // Undead:
+    private static final Map<EntityType, Enchantment> enchants = ImmutableMap.<EntityType, Enchantment>builder()
+
+            // Undead (https://minecraft.gamepedia.com/Undead)
             .put(EntityType.SKELETON, Enchantment.DAMAGE_UNDEAD)
             .put(EntityType.ZOMBIE, Enchantment.DAMAGE_UNDEAD)
             .put(EntityType.WITHER, Enchantment.DAMAGE_UNDEAD)
-            .put(EntityType.PIG_ZOMBIE, Enchantment.DAMAGE_UNDEAD)
-            // Arthropods:
+            .put(EntityType.WITHER_SKELETON, Enchantment.DAMAGE_UNDEAD)
+            .put(EntityType.ZOMBIFIED_PIGLIN, Enchantment.DAMAGE_UNDEAD)
+            .put(EntityType.SKELETON_HORSE, Enchantment.DAMAGE_UNDEAD)
+            .put(EntityType.STRAY, Enchantment.DAMAGE_UNDEAD)
+            .put(EntityType.HUSK, Enchantment.DAMAGE_UNDEAD)
+            .put(EntityType.PHANTOM, Enchantment.DAMAGE_UNDEAD)
+            .put(EntityType.DROWNED, Enchantment.DAMAGE_UNDEAD)
+            .put(EntityType.ZOGLIN, Enchantment.DAMAGE_UNDEAD)
+
+            // Arthropods (https://minecraft.gamepedia.com/Arthropod)
             .put(EntityType.SPIDER, Enchantment.DAMAGE_ARTHROPODS)
             .put(EntityType.CAVE_SPIDER, Enchantment.DAMAGE_ARTHROPODS)
+            .put(EntityType.BEE, Enchantment.DAMAGE_ARTHROPODS)
             .put(EntityType.SILVERFISH, Enchantment.DAMAGE_ARTHROPODS)
             .put(EntityType.ENDERMITE, Enchantment.DAMAGE_ARTHROPODS)
+
             .build();
 
     public static double applyEntityBasedDamage(EntityType entity, ItemStack item, double startDamage){
-        Enchantment ench = enchants.get(entity);
+        Enchantment enchantment = enchants.get(entity);
 
-        if(ench == null){
-            return startDamage;
-        }
+        if(enchantment == null) return startDamage;
 
-        if(ench == Enchantment.DAMAGE_UNDEAD || ench == Enchantment.DAMAGE_ARTHROPODS){
-            return startDamage + 2.5 * item.getEnchantmentLevel(ench);
-        }
+        if(enchantment == Enchantment.DAMAGE_UNDEAD || enchantment == Enchantment.DAMAGE_ARTHROPODS)
+            return startDamage + 2.5 * item.getEnchantmentLevel(enchantment);
 
         return startDamage;
     }
