35ed29252626c5bb93481834f4e4c4d329d269d8
kernitus <2789734+kernitus@users.noreply.github.com>
2020-12-21 22:38:51 +0100

    Fix OldPlayerRegen not accounting for exhaustion changes when cancelling event & not healing, and not healing on first foodTickTimer expiry

--- src/main/java/kernitus/plugin/OldCombatMechanics/module/ModulePlayerRegen.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/module/ModulePlayerRegen.java
@@ -18,75 +18,81 @@ import java.util.UUID;
 
 /**
  * Establishes custom health regeneration rules.
+ * Default values based on 1.8 from https://minecraft.gamepedia.com/Hunger?oldid=948685
  */
 public class ModulePlayerRegen extends Module {
 
     private final Map<UUID, Long> healTimes = new HashMap<>();
     private boolean spartanInstalled;
 
-    public ModulePlayerRegen(OCMMain plugin){
+    public ModulePlayerRegen(OCMMain plugin) {
         super(plugin, "old-player-regen");
 
         initSpartan();
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onRegen(EntityRegainHealthEvent e){
+    public void onRegen(EntityRegainHealthEvent e) {
 
-        if(e.getEntityType() != EntityType.PLAYER || e.getRegainReason() != EntityRegainHealthEvent.RegainReason.SATIATED){
+        if (e.getEntityType() != EntityType.PLAYER
+                || e.getRegainReason() != EntityRegainHealthEvent.RegainReason.SATIATED)
             return;
-        }
 
         final Player p = (Player) e.getEntity();
 
-        if(!isEnabled(p.getWorld())) return;
+        if (!isEnabled(p.getWorld())) return;
+        final UUID playerId = p.getUniqueId();
 
+        // We cancel the regen, but saturation and exhaustion need to be adjusted separately
+        // Exhaustion is modified in the next tick, and saturation in the tick following that (if exhaustion > 4)
         e.setCancelled(true);
 
-        long currentTime = System.currentTimeMillis() / 1000;
-        long lastHealTime = getLastHealTime(p);
+        // Get exhaustion & saturation values before healing modifies them
+        final float previousExhaustion = p.getExhaustion();
+        final float previousSaturation = p.getSaturation();
 
-        if(currentTime - lastHealTime < module().getLong("frequency"))
+        // Check that it has been at least x seconds since last heal
+        final long currentTime = System.currentTimeMillis();
+        final boolean hasLastHealTime = healTimes.containsKey(playerId);
+        final long lastHealTime = healTimes.computeIfAbsent(playerId, id -> currentTime);
+
+        // If we're skipping this heal, we must fix the exhaustion in the following tick
+        if (hasLastHealTime && currentTime - lastHealTime < module().getLong("interval")) {
+            Bukkit.getScheduler().runTaskLater(plugin, () -> p.setExhaustion(previousExhaustion), 1L);
             return;
-
-        double maxHealth = p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
-
-        if(p.getHealth() < maxHealth){
-            p.setHealth(MathHelper.clamp(p.getHealth() + module().getInt("amount"), 0.0, maxHealth));
-            healTimes.put(p.getUniqueId(), currentTime);
-
-            disableSpartanRegenCheck(p);
         }
 
-        final float previousExhaustion = p.getExhaustion();
+        final double maxHealth = p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+        final double playerHealth = p.getHealth();
+
+        if (playerHealth < maxHealth) {
+            p.setHealth(MathHelper.clamp(playerHealth + module().getInt("amount"), 0.0, maxHealth));
+            healTimes.put(playerId, currentTime);
+            if(spartanInstalled) disableSpartanRegenCheck(p);
+        }
+
+        // Calculate new exhaustion value, must be between 0 and 4. If above, it will reduce the saturation in the following tick.
         final float exhaustionToApply = (float) module().getDouble("exhaustion");
 
         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            //This is because bukkit doesn't stop the exhaustion change when cancelling the event
+            // We do this in the next tick because bukkit doesn't stop the exhaustion change when cancelling the event
             p.setExhaustion(previousExhaustion + exhaustionToApply);
-            debug("Exhaustion before: " + previousExhaustion + " Now: " + p.getExhaustion() + " Saturation: " + p.getSaturation(), p);
+            debug("Exhaustion before: " + previousExhaustion + " Now: " + p.getExhaustion() +
+                    " Saturation now: " + previousSaturation, p);
         }, 1L);
     }
-    
+
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent e){
+    public void onPlayerQuit(PlayerQuitEvent e) {
         healTimes.remove(e.getPlayer().getUniqueId());
     }
 
-    private long getLastHealTime(Player p){
-        return healTimes.computeIfAbsent(p.getUniqueId(), id -> System.currentTimeMillis() / 1000);
-    }
-
-    private void disableSpartanRegenCheck(Player player){
-        if(!spartanInstalled){
-            return;
-        }
-
-        int ticksToCancel = plugin.getConfig().getInt("support.spartan-cancel-ticks", 1);
+    private void disableSpartanRegenCheck(Player player) {
+        final int ticksToCancel = plugin.getConfig().getInt("support.spartan-cancel-ticks", 1);
         me.vagdedes.spartan.api.API.cancelCheck(player, Enums.HackType.FastHeal, ticksToCancel);
     }
 
-    private void initSpartan(){
+    private void initSpartan() {
         spartanInstalled = Bukkit.getPluginManager().getPlugin("Spartan") != null;
     }
 }
