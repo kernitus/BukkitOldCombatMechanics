775dda92f5791988b93786b0f931a2bf48761a46
kernitus <2789734+kernitus@users.noreply.github.com>
2020-12-22 13:52:10 +0100

    Add message for disabled crafting. Closes #471

--- src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleDisableCrafting.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleDisableCrafting.java
@@ -2,8 +2,10 @@ package kernitus.plugin.OldCombatMechanics.module;
 
 import kernitus.plugin.OldCombatMechanics.OCMMain;
 import kernitus.plugin.OldCombatMechanics.utilities.ConfigUtils;
+import kernitus.plugin.OldCombatMechanics.utilities.Messenger;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
@@ -18,6 +20,7 @@ import java.util.List;
 public class ModuleDisableCrafting extends Module {
 
     private List<Material> denied;
+    private String message;
 
     public ModuleDisableCrafting(OCMMain plugin){
         super(plugin, "disable-crafting");
@@ -27,19 +30,23 @@ public class ModuleDisableCrafting extends Module {
     @Override
     public void reload(){
         denied = ConfigUtils.loadMaterialList(module(), "denied");
+        message = module().getBoolean("showMessage") ? module().getString("message") : null;
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPrepareItemCraft(PrepareItemCraftEvent e){
-        if(e.getViewers().size() < 1) return;
+        final List<HumanEntity> viewers = e.getViewers();
+        if(viewers.size() < 1) return;
 
-        World world = e.getViewers().get(0).getWorld();
+        final World world = viewers.get(0).getWorld();
         if(!isEnabled(world)) return;
 
-        CraftingInventory inv = e.getInventory();
-        ItemStack result = inv.getResult();
+        final CraftingInventory inv = e.getInventory();
+        final ItemStack result = inv.getResult();
 
-        if(result != null && denied.contains(result.getType()))
+        if(result != null && denied.contains(result.getType())) {
             inv.setResult(null);
+            if (message != null) viewers.forEach(viewer -> Messenger.sendNormalMessage(viewer,message));
+        }
     }
 }
