6f0c7907adf254addd7181f06506c354ed21c420
kernitus <2789734+kernitus@users.noreply.github.com>
2020-12-24 15:08:05 +0100

    Clean up OCM command handling

--- src/main/java/kernitus/plugin/OldCombatMechanics/OCMCommandHandler.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/OCMCommandHandler.java
@@ -9,8 +9,10 @@ import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.Locale;
@@ -18,26 +20,29 @@ import java.util.Locale;
 public class OCMCommandHandler implements CommandExecutor {
     private static final String NO_PERMISSION = "&cYou need the permission '%s' to do that!";
 
-    private OCMMain plugin;
-    private File pluginFile;
+    private final OCMMain plugin;
+    private final File pluginFile;
 
-    public OCMCommandHandler(OCMMain instance, File pluginFile){
+    public OCMCommandHandler(OCMMain instance, File pluginFile) {
         this.plugin = instance;
         this.pluginFile = pluginFile;
     }
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-        if(args.length < 1){//Tell them about available commands
-            PluginDescriptionFile pdf = plugin.getDescription();
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+
+        final FileConfiguration config = plugin.getConfig();
+
+        // Show them the available commands
+        if (args.length < 1) {
+            final PluginDescriptionFile description = plugin.getDescription();
 
             Messenger.send(sender, ChatColor.DARK_GRAY + Messenger.HORIZONTAL_BAR);
-
-            Messenger.send(sender, "&6&lOldCombatMechanics&e by &ckernitus&e and &cRayzr522&e version &6%s", pdf.getVersion());
+            Messenger.send(sender, "&6&lOldCombatMechanics&e by &ckernitus&e and &cRayzr522&e version &6%s", description.getVersion());
             Messenger.send(sender, "&eYou can use &c/ocm reload&e to reload the config file");
 
-            if(plugin.getConfig().getBoolean("enableIndividualToggle") && sender.hasPermission("oldcombatmechanics.toggle") && sender instanceof Player){
+            if (sender instanceof Player && sender.hasPermission("oldcombatmechanics.toggle")
+                    && config.getBoolean("enableIndividualToggle"))
                 Messenger.send(sender, "&eYou can use &c/ocm toggle&e to turn your attack cooldown on/off");
-            }
 
             Messenger.send(sender, ChatColor.DARK_GRAY + Messenger.HORIZONTAL_BAR);
 
@@ -47,44 +52,40 @@ public class OCMCommandHandler implements CommandExecutor {
             return true;
         }
 
-        // Get the sub-command
-        String sub = args[0].toLowerCase(Locale.ROOT);
+        // Otherwise, get the subcommand
+        final String subcommand = args[0].toLowerCase(Locale.ROOT);
 
-        if(sub.equals("reload")){// Reloads config
-            if(!sender.hasPermission("oldcombatmechanics.reload")){
-                Messenger.send(sender, NO_PERMISSION, "oldcombatmechanics.reload");
-                return true;
-            }
-
-            Config.reload();
-
-            Messenger.send(sender, "&6&lOldCombatMechanics&e config file reloaded");
+        if (subcommand.equals("reload")) {
+            if (sender.hasPermission("oldcombatmechanics.reload")) {
+                Config.reload();
+                Messenger.send(sender, "&6&lOldCombatMechanics&e config file reloaded");
+            } else Messenger.send(sender, NO_PERMISSION, "oldcombatmechanics.reload");
 
             return true;
-        } else if(sub.equals("toggle") && plugin.getConfig().getBoolean("enableIndividualToggle") && sender instanceof Player){
-            if(!sender.hasPermission("oldcombatmechanics.toggle")){
+        } else if (sender instanceof Player && config.getBoolean("enableIndividualToggle") && subcommand.equals("toggle")) {
+            if (!sender.hasPermission("oldcombatmechanics.toggle")) {
                 Messenger.send(sender, NO_PERMISSION, "oldcombatmechanics.toggle");
                 return true;
             }
 
             // Toggle their cooldown
-            Player p = (Player) sender;
-            double GAS = plugin.getConfig().getDouble("disable-attack-cooldown.generic-attack-speed");
+            final Player player = (Player) sender;
+            double speed = config.getDouble("disable-attack-cooldown.generic-attack-speed");
 
-            AttributeInstance attribute = p.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
-            double baseValue = attribute.getBaseValue();
+            final AttributeInstance attribute = player.getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+            final double baseValue = attribute.getBaseValue();
             String message = "&1[OCM] &aAttack cooldown ";
 
-            if(baseValue == GAS){ // Toggle
-                GAS = 4;
+            if (baseValue == speed) { // Toggle
+                speed = 4;
                 message += "enabled";
             } else {
                 message += "disabled";
             }
 
-            attribute.setBaseValue(GAS);
-            p.saveData();
-            Messenger.send(p, message);
+            attribute.setBaseValue(speed);
+            player.saveData();
+            Messenger.send(player, message);
 
             return true;
         }
