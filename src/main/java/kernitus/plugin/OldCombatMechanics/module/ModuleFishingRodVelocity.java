962ab4a943b70024c267592c487285fcb7070853
kernitus <2789734+kernitus@users.noreply.github.com>
2020-12-24 18:53:19 +0100

    Clean up FishingRodKnockback & use internal utilities

--- src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleFishingRodVelocity.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleFishingRodVelocity.java
@@ -1,52 +1,46 @@
 package kernitus.plugin.OldCombatMechanics.module;
 
 import kernitus.plugin.OldCombatMechanics.OCMMain;
+import kernitus.plugin.OldCombatMechanics.utilities.reflection.Reflector;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.FishHook;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.util.Vector;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Random;
 
+/**
+ * This module reverts fishing rod gravity and velocity back to 1.8 behaviour
+ * <p>
+ * Fishing rod gravity in 1.14+ is 0.04 while in 1.8 it is 0.03
+ * Launch velocity in 1.9+ is also changed from the 1.8 formula
+ */
 public class ModuleFishingRodVelocity extends Module {
-    public static HashSet<FishHook> fishHooks = new HashSet<>();
-    public static boolean versionChagesGravity;
-    Random rand = new Random();
-    Method getHook;
 
-    /**
-     * This module uses the exact 1.8 formula in order to revert fishing rod gravity
-     * and fishing rod velocity back to 1.8
-     *
-     * Fishing rod gravity in 1.14+ is 0.04 while in 1.8 it is 0.03
-     * Launch velocity in 1.9+ is also changed from the 1.8 formula
-     */
+    private static final HashSet<FishHook> fishHooks = new HashSet<>();
+    private static boolean hasDifferentGravity;
+    private final Random random = new Random();
+    private final Method getHook;
 
     public ModuleFishingRodVelocity(OCMMain plugin) {
         super(plugin, "fishing-rod-velocity");
 
-        // 1.14 versions and higher change gravity in testing, while 1.13 versions and lower
-        // have the same fishing hook gravity as 1.8
-        versionChagesGravity = !Bukkit.getVersion().contains("1.13") && !Bukkit.getVersion().contains("1.12") && !Bukkit.getVersion().contains("1.11") && !Bukkit.getVersion().contains("1.10") && !Bukkit.getVersion().contains("1.9");
+        // Versions 1.14+ have different gravity than previous versions
+        final String bukkitVersion = Bukkit.getBukkitVersion();
+        hasDifferentGravity = Reflector.versionIsNewerOrEqualAs(1, 14, 0);
 
-        try {
-            // Reflection because in 1.12- this method returns the Fish class, which was renamed to FishHook in 1.13+
-            getHook = PlayerFishEvent.class.getMethod("getHook");
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
+        // Reflection because in 1.12- this method returns the Fish class, which was renamed to FishHook in 1.13+
+        getHook = Reflector.getMethod(PlayerFishEvent.class, "getHook");
 
-        if (versionChagesGravity) {
-            OCMMain.getInstance().addEnableListener(() -> Bukkit.getScheduler().runTaskTimer(OCMMain.getInstance(), () -> {
+        if (hasDifferentGravity) {
+            plugin.addEnableListener(() -> Bukkit.getScheduler().runTaskTimer(plugin, () -> {
 
                 Iterator<FishHook> fishingHookIterator = ModuleFishingRodVelocity.fishHooks.iterator();
 
@@ -66,53 +60,48 @@ public class ModuleFishingRodVelocity extends Module {
     }
 
     @EventHandler
-    public void onFishEvent(PlayerFishEvent event) throws InvocationTargetException, IllegalAccessException {
-        FishHook fishHook = (FishHook) getHook.invoke(event);
+    public void onFishEvent(PlayerFishEvent event) {
+        final FishHook fishHook = Reflector.invokeMethod(getHook, event);
 
-        if(!isEnabled(fishHook.getWorld())) return;
+        if (!isEnabled(fishHook.getWorld()) || event.getState() != PlayerFishEvent.State.FISHING) return;
 
-        if (event.getState() == PlayerFishEvent.State.FISHING) {
-            Player casterPlayer = event.getPlayer();
-            double playerYaw = casterPlayer.getLocation().getYaw();
-            double playerPitch = casterPlayer.getLocation().getPitch();
+        Player casterPlayer = event.getPlayer();
+        double playerYaw = casterPlayer.getLocation().getYaw();
+        double playerPitch = casterPlayer.getLocation().getPitch();
 
-            float oldMaxVelocity = 0.4F;
-            double velocityX = -Math.sin(playerYaw / 180.0F * (float) Math.PI) * Math.cos(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
-            double velocityZ = Math.cos(playerYaw / 180.0F * (float) Math.PI) * Math.cos(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
-            double velocityY = -Math.sin(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
+        float oldMaxVelocity = 0.4F;
+        double velocityX = -Math.sin(playerYaw / 180.0F * (float) Math.PI) * Math.cos(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
+        double velocityZ = Math.cos(playerYaw / 180.0F * (float) Math.PI) * Math.cos(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
+        double velocityY = -Math.sin(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
 
-            double oldVelocityMultiplier = 1.5;
+        double oldVelocityMultiplier = 1.5;
 
-            float vectorLength = (float) Math.sqrt(velocityX * velocityX + velocityY * velocityY + velocityZ * velocityZ);
-            velocityX = velocityX / (double) vectorLength;
-            velocityY = velocityY / (double) vectorLength;
-            velocityZ = velocityZ / (double) vectorLength;
-            velocityX = velocityX + this.rand.nextGaussian() * 0.007499999832361937D;
-            velocityY = velocityY + this.rand.nextGaussian() * 0.007499999832361937D;
-            velocityZ = velocityZ + this.rand.nextGaussian() * 0.007499999832361937D;
-            velocityX = velocityX * oldVelocityMultiplier;
-            velocityY = velocityY * oldVelocityMultiplier;
-            velocityZ = velocityZ * oldVelocityMultiplier;
+        float vectorLength = (float) Math.sqrt(velocityX * velocityX + velocityY * velocityY + velocityZ * velocityZ);
+        velocityX = velocityX / (double) vectorLength;
+        velocityY = velocityY / (double) vectorLength;
+        velocityZ = velocityZ / (double) vectorLength;
+        velocityX = velocityX + this.random.nextGaussian() * 0.007499999832361937D;
+        velocityY = velocityY + this.random.nextGaussian() * 0.007499999832361937D;
+        velocityZ = velocityZ + this.random.nextGaussian() * 0.007499999832361937D;
+        velocityX = velocityX * oldVelocityMultiplier;
+        velocityY = velocityY * oldVelocityMultiplier;
+        velocityZ = velocityZ * oldVelocityMultiplier;
 
-            Vector newSpeed = new Vector(velocityX, velocityY, velocityZ);
+        Vector newSpeed = new Vector(velocityX, velocityY, velocityZ);
 
-            fishHook.setVelocity(newSpeed);
-        }
+        fishHook.setVelocity(newSpeed);
     }
 
-
     @EventHandler(priority = EventPriority.MONITOR)
-    public void onFishingEventMonitor(PlayerFishEvent event) throws InvocationTargetException, IllegalAccessException {
-        FishHook fishHook = (FishHook) getHook.invoke(event);
+    public void onFishingEventMonitor(PlayerFishEvent event) {
+        final FishHook fishHook = Reflector.invokeMethod(getHook, event);
 
-        if(!isEnabled(fishHook.getWorld())) return;
+        if (!isEnabled(fishHook.getWorld())) return;
 
-        if (versionChagesGravity && event.getState() == PlayerFishEvent.State.FISHING) {
-            fishHooks.add(fishHook);
-        }
+        final PlayerFishEvent.State state = event.getState();
 
-        if (event.getState()  == PlayerFishEvent.State.IN_GROUND) {
-            fishHooks.remove(fishHook);
-        }
+        if (hasDifferentGravity && state == PlayerFishEvent.State.FISHING) fishHooks.add(fishHook);
+
+        if (state == PlayerFishEvent.State.IN_GROUND) fishHooks.remove(fishHook);
     }
 }
