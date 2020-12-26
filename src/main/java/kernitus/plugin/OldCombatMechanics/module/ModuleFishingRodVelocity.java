570059ccedd435696cfe569f00eeb9f1996a39b7
kernitus <2789734+kernitus@users.noreply.github.com>
2020-12-26 11:43:30 +0100

    Remove unused variable & fix gravity comment

--- src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleFishingRodVelocity.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleFishingRodVelocity.java
@@ -3,9 +3,9 @@ package kernitus.plugin.OldCombatMechanics.module;
 import kernitus.plugin.OldCombatMechanics.OCMMain;
 import kernitus.plugin.OldCombatMechanics.utilities.reflection.Reflector;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.FishHook;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerFishEvent;
@@ -19,7 +19,7 @@ import java.util.Random;
 /**
  * This module reverts fishing rod gravity and velocity back to 1.8 behaviour
  * <p>
- * Fishing rod gravity in 1.14+ is 0.04 while in 1.8 it is 0.03
+ * Fishing rod gravity in 1.14+ is 0.03 while in 1.8 it is 0.04
  * Launch velocity in 1.9+ is also changed from the 1.8 formula
  */
 public class ModuleFishingRodVelocity extends Module {
@@ -33,7 +33,6 @@ public class ModuleFishingRodVelocity extends Module {
         super(plugin, "fishing-rod-velocity");
 
         // Versions 1.14+ have different gravity than previous versions
-        final String bukkitVersion = Bukkit.getBukkitVersion();
         hasDifferentGravity = Reflector.versionIsNewerOrEqualAs(1, 14, 0);
 
         // Reflection because in 1.12- this method returns the Fish class, which was renamed to FishHook in 1.13+
@@ -65,29 +64,31 @@ public class ModuleFishingRodVelocity extends Module {
 
         if (!isEnabled(fishHook.getWorld()) || event.getState() != PlayerFishEvent.State.FISHING) return;
 
-        Player casterPlayer = event.getPlayer();
-        double playerYaw = casterPlayer.getLocation().getYaw();
-        double playerPitch = casterPlayer.getLocation().getPitch();
+        final Location location = event.getPlayer().getLocation();
+        final double playerYaw = location.getYaw();
+        final double playerPitch = location.getPitch();
 
-        float oldMaxVelocity = 0.4F;
+        final float oldMaxVelocity = 0.4F;
         double velocityX = -Math.sin(playerYaw / 180.0F * (float) Math.PI) * Math.cos(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
         double velocityZ = Math.cos(playerYaw / 180.0F * (float) Math.PI) * Math.cos(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
         double velocityY = -Math.sin(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
 
-        double oldVelocityMultiplier = 1.5;
+        final double oldVelocityMultiplier = 1.5;
 
-        float vectorLength = (float) Math.sqrt(velocityX * velocityX + velocityY * velocityY + velocityZ * velocityZ);
-        velocityX = velocityX / (double) vectorLength;
-        velocityY = velocityY / (double) vectorLength;
-        velocityZ = velocityZ / (double) vectorLength;
-        velocityX = velocityX + this.random.nextGaussian() * 0.007499999832361937D;
-        velocityY = velocityY + this.random.nextGaussian() * 0.007499999832361937D;
-        velocityZ = velocityZ + this.random.nextGaussian() * 0.007499999832361937D;
-        velocityX = velocityX * oldVelocityMultiplier;
-        velocityY = velocityY * oldVelocityMultiplier;
-        velocityZ = velocityZ * oldVelocityMultiplier;
+        final double vectorLength = (float) Math.sqrt(velocityX * velocityX + velocityY * velocityY + velocityZ * velocityZ);
+        velocityX /= vectorLength;
+        velocityY /= vectorLength;
+        velocityZ /= vectorLength;
 
-        Vector newSpeed = new Vector(velocityX, velocityY, velocityZ);
+        velocityX += random.nextGaussian() * 0.007499999832361937D;
+        velocityY += random.nextGaussian() * 0.007499999832361937D;
+        velocityZ += random.nextGaussian() * 0.007499999832361937D;
+
+        velocityX *= oldVelocityMultiplier;
+        velocityY *= oldVelocityMultiplier;
+        velocityZ *= oldVelocityMultiplier;
+
+        final Vector newSpeed = new Vector(velocityX, velocityY, velocityZ);
 
         fishHook.setVelocity(newSpeed);
     }
