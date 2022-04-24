6fda20ad68453c1ff834b4e556e6417e1b427fe1
kernitus <2789734+kernitus@users.noreply.github.com>
2022-04-24 18:07:29 +0200

    Don't use rotateAroundY in <1.14. Fixes #606

--- src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleDisableProjectileRandomness.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/module/ModuleDisableProjectileRandomness.java
@@ -1,6 +1,7 @@
 package kernitus.plugin.OldCombatMechanics.module;
 
 import kernitus.plugin.OldCombatMechanics.OCMMain;
+import kernitus.plugin.OldCombatMechanics.utilities.reflection.Reflector;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
@@ -14,24 +15,25 @@ import org.bukkit.util.Vector;
 public class ModuleDisableProjectileRandomness extends Module {
 
     private static double EPSILON;
-    public ModuleDisableProjectileRandomness(OCMMain plugin){
+
+    public ModuleDisableProjectileRandomness(OCMMain plugin) {
         super(plugin, "disable-projectile-randomness");
         reload();
     }
 
     @Override
-    public void reload(){
+    public void reload() {
         EPSILON = module().getDouble("epsilon");
     }
 
     @EventHandler
-    public void onProjectileLaunch(ProjectileLaunchEvent e){
+    public void onProjectileLaunch(ProjectileLaunchEvent e) {
         Projectile projectile = e.getEntity();
         ProjectileSource shooter = projectile.getShooter();
 
-        if(shooter instanceof Player){
+        if (shooter instanceof Player) {
             Player player = (Player) shooter;
-            if(!isEnabled(player.getWorld())) return;
+            if (!isEnabled(player.getWorld())) return;
             debug("Making projectile go straight", player);
 
             Vector playerDirection = player.getLocation().getDirection().normalize();
@@ -44,11 +46,10 @@ public class ModuleDisableProjectileRandomness extends Module {
             // The following works because using rotate modifies the vector, so we must double it to undo the rotation
             // The vector is rotated around the Y axis and matched by checking only the X and Z values
             // Angles is specified in radians, where 10° = 0.17 radians
-            if(!fuzzyVectorEquals(projectileDirection, playerDirection)) { // If the projectile is not going straight
-                if (fuzzyVectorEquals(projectileDirection, playerDirection.rotateAroundY(0.17))) {
+            if (!fuzzyVectorEquals(projectileDirection, playerDirection)) { // If the projectile is not going straight
+                if (fuzzyVectorEquals(projectileDirection, rotateAroundY(playerDirection, 0.17))) {
                     debug("10° Offset", player);
-                }
-                else if (fuzzyVectorEquals(projectileDirection, playerDirection.rotateAroundY(-0.35)))
+                } else if (fuzzyVectorEquals(projectileDirection, rotateAroundY(playerDirection, -0.35)))
                     //arrowVelocity.rotateAroundY(-10);
                     debug("-10° Offset", player);
             }
@@ -58,8 +59,21 @@ public class ModuleDisableProjectileRandomness extends Module {
         }
     }
 
-    private boolean fuzzyVectorEquals(Vector a, Vector b){
+    private boolean fuzzyVectorEquals(Vector a, Vector b) {
         return Math.abs(a.getX() - b.getX()) < EPSILON &&
                 Math.abs(a.getZ() - b.getZ()) < EPSILON;
+    }
+
+    private Vector rotateAroundY(Vector vector, double angle) {
+        if (Reflector.versionIsNewerOrEqualAs(14, 0, 0))
+            return vector.rotateAroundY(angle);
+        else {
+            double angleCos = Math.cos(angle);
+            double angleSin = Math.sin(angle);
+
+            double x = angleCos * vector.getX() + angleSin * vector.getZ();
+            double z = -angleSin * vector.getX() + angleCos * vector.getZ();
+            return vector.setX(x).setZ(z);
+        }
     }
 }
