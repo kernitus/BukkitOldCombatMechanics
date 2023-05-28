1a27589565434be2cf028bc32c84f12ea0cc1584
kernitus <2789734+kernitus@users.noreply.github.com>
2023-05-28 16:49:51 +0100

    Switch from Maven to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    `java-library`
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+repositories {
+    mavenCentral()
+    // Spigot API
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
+    // Placeholder API for reflection utils
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    // CodeMC Repo for bStats
+    maven("https://repo.codemc.org/repository/maven-public/")
+    // Netty library from Minecraft
+    maven("https://libraries.minecraft.net/")
+    // Local copy of Spartan API
+    maven("file:///home/yee/IdeaProjects/BukkitOldCombatMechanics/internal-file-repo")
+}
+
+dependencies {
+    implementation("org.bstats:bstats-bukkit:3.0.2")
+    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+    // Shaded in by Bukkit
+    compileOnly("io.netty:netty-all:4.1.93.Final")
+    compileOnly("me.clip:placeholderapi:2.11.3")
+    // Spartan API
+    compileOnly("me.vagdedes.spartan:SpartanAPI:200")
+}
+
+group = "kernitus.plugin.OldCombatMechanics"
+version = "1.12.2-beta"
+description = "OldCombatMechanics"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            exclude("kernitus/plugin/OldCombatMechanics/tester/**")
+        }
+    }
+}
+
+// Substitute ${pluginVersion} in plugin.yml with version defined above
+tasks.named<Copy>("processResources") {
+    inputs.property("pluginVersion", version)
+    filesMatching("plugin.yml") {
+        expand("pluginVersion" to version)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.named("assemble").configure {
+    dependsOn("shadowJar")
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    archiveBaseName.set("OldCombatMechanics")
+    dependencies {
+        relocate("org.bstats", "kernitus.plugin.OldCombatMechanics.lib.bstats")
+    }
+}
