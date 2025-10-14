8d88a49d0fa1e8c7dfa7c48cf66c8399c766d6e0
kernitus <2789734+kernitus@users.noreply.github.com>
2025-10-14 14:42:45 +0100

    fix: update checker versioning logic

--- src/main/java/kernitus/plugin/OldCombatMechanics/updater/VersionChecker.java
+++ src/main/java/kernitus/plugin/OldCombatMechanics/updater/VersionChecker.java
@@ -29,23 +29,20 @@ public class VersionChecker {
     }
 
     private static int[] getVersionNumbers(String ver) {
-        Matcher m = Pattern.compile("(\\d+)\\.(\\d+)\\.*(\\d*)(-beta(\\d*))?", Pattern.CASE_INSENSITIVE).matcher(ver);
-        if (!m.matches()) throw new IllegalArgumentException("Plugin version formatted wrong!");
+        // Support both -beta and -SNAPSHOT
+        Matcher m = Pattern
+                .compile("(\\d+)\\.(\\d+)(?:\\.(\\d+))?(?:-(beta|SNAPSHOT)(\\d*))?", Pattern.CASE_INSENSITIVE)
+                .matcher(ver);
+        if (!m.matches())
+            throw new IllegalArgumentException("Plugin version formatted wrong!");
 
-        // Group 1 = MAJOR
-        // Group 2 = MINOR
-        // Group 3 = PATCH
-        // Group 4 = beta
-        // Group 5 = beta_version
-
-        //This parses it to MAJOR.MINOR.PATCH-beta_version
-        //MAJOR & MINOR required, anything else is set to maximum value if omitted - necessary otherwise
-        // somebody with a beta version will not see update to release version
-        return new int[]{
-                Integer.parseInt(m.group(1)),
-                Integer.parseInt(m.group(2)),
-                m.group(3).isEmpty() ? Integer.MAX_VALUE : Integer.parseInt(m.group(3)),
-                m.group(4) == null ? Integer.MAX_VALUE : (m.group(5).isEmpty() ? 1 : Integer.parseInt(m.group(5)))
+        return new int[] {
+                Integer.parseInt(m.group(1)), // MAJOR
+                Integer.parseInt(m.group(2)), // MINOR
+                m.group(3) == null ? 0 : Integer.parseInt(m.group(3)), // PATCH (default 0)
+                m.group(4) == null ? Integer.MAX_VALUE : // Release version
+                        (m.group(5).isEmpty() ? 0 : Integer.parseInt(m.group(5))) // Pre-release number
         };
     }
+
 }
