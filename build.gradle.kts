ed3cd580fd7e896164a570da2c08c1711b5ba52c
kernitus <2789734+kernitus@users.noreply.github.com>
2026-06-27 19:08:31 +0100

    chore(deps): bump PacketEvents to 2.13.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -154,7 +154,7 @@ dependencies {
     // JSR-305 annotations (javax.annotation.Nullable)
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     // PacketEvents
-    implementation("com.github.retrooper:packetevents-spigot:2.12.2")
+    implementation("com.github.retrooper:packetevents-spigot:2.13.0")
     // XSeries
     implementation("com.github.cryptomorin:XSeries:13.7.0")
 
